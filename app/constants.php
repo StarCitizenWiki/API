d416beab60da6822e56ecc79bec7f0a7ee727669
Keonie <keonie@star-citizen.wiki>
2017-02-01 00:22:11 +0100

    FunfImageController mit Variablensteuerung, Globale constants.php hinzugefuegt

---
+++ app/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Global Constants
+ */
+
+define('FUNDIMAGE_FUNDING_ONLY',  'funding_only');
+define('FUNDIMAGE_FUNDING_AND_TEXT',  'funding_and_text');
+define('FUNDIMAGE_FUNDING_AND_BARS',  'funding_and_bars');
--- routes/web.php
+++ routes/web.php
@@ -42,8 +42,8 @@ Route::group(['namespace' => 'Tools'], function () {
         Route::group(['prefix' => 'images'], function () {
             Route::get('funds', ['uses' => 'FundImageController@getImage']);
             Route::group(['prefix' => 'funds'], function () {
-                Route::get('text', ['uses' => 'FundImageController@getImageWithText']);
-                Route::get('bar', ['uses' => 'FundImageController@getImageWithBars']);
+                Route::get('text', ['uses' => 'FundImageController@getImage', 'type' => FUNDIMAGE_FUNDING_AND_TEXT]);
+                Route::get('bar', ['uses' => 'FundImageController@getImage', 'type' => FUNDIMAGE_FUNDING_AND_BARS]);
             });
         });
 
