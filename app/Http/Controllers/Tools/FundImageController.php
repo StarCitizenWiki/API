0d20fce5f86ad27cfd1744145a0b8bc4b8570c88
FoXFTW <hannes@octofox.de>
2017-02-01 14:40:35 +0100

    Add Barimage generation to FundImageController update Tests

--- app/Http/Controllers/Tools/FundImageController.php
+++ app/Http/Controllers/Tools/FundImageController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Tools;
 use App\Exceptions\InvalidDataException;
 use App\Exceptions\MissingExtensionException;
 use App\Repositories\StarCitizen\APIv1\Stats\StatsRepository;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 
 class FundImageController extends Controller
 {
-    const COLORS = ['blue' => [0, 231, 255], 'black' => [51, 51, 51]];
+    const COLORS = ['blue' => [0, 231, 255], 'darkblue' => [69, 117, 129], 'black' => [51, 51, 51]];
     const FUNDING_ONLY = FUNDIMAGE_FUNDING_ONLY;
     const FUNDING_AND_TEXT = FUNDIMAGE_FUNDING_AND_TEXT;
     const FUNDING_AND_BARS = FUNDIMAGE_FUNDING_AND_BARS;
@@ -22,14 +23,22 @@ class FundImageController extends Controller
 		FundImageController::FUNDING_AND_BARS
 	];
 
+	private $_request;
     private $_api;
-    private $_funds;
+    private $_funds = [
+        'current' => null,
+        'currentFormatted' => null,
+        'nextMillion' => null,
+        'nextMillionFormatted' => null,
+        'percentageToNextMillion' => null,
+        'substractor' => null,
+    ];
     private $_image = [
         'pointer' => null,
         'width' => null,
         'height' => null,
         'data' => null,
-        'type' => FundImageController::FUNDING_ONLY,
+        'type' => null,
         'text' => 'Crowdfunding:',
         'name' => null
     ];
@@ -38,9 +47,10 @@ class FundImageController extends Controller
         'color' => null,
     ];
 
-    public function __construct(StatsRepository $api)
+    public function __construct(Request $request, StatsRepository $api)
     {
         $this->_checkIfImageCanBeCreated();
+        $this->_request = $request;
         $this->_api = $api;
         $this->_font['path'] = resource_path('assets/fonts/orbitron-light-webfont.ttf');
         $this->_font['color'] = FundImageController::COLORS['black'];
@@ -68,7 +78,6 @@ class FundImageController extends Controller
         return $this->getImage();
     }
 
-
     /**
      * Generates the Image with the defined Values
      *
@@ -91,38 +100,17 @@ class FundImageController extends Controller
         try {
             $this->_getFundsFromAPI();
             $this->_formatFunds();
+            $this->_determineImageWidth();
             $this->_determineImageHeight();
             $this->_initImage();
             $this->_addDataToImage();
             $this->_flushImageToString();
             $this->_saveImageToDisk();
-        } catch (InvalidDataException $e) {
+        } catch (\Exception $e) {
             // @TODO Logging und Mailversand
-        } finally {
-            return $this->_loadImageFromDisk();
         }
-    }
 
-    /**
-     * @deprecated Farbe über Request ermitteln
-     *
-     * @return FundImageController
-     */
-    public function blackColor() : FundImageController
-    {
-        $this->_font['color'] = FundImageController::COLORS['black'];
-        return $this;
-    }
-
-    /**
-     * @deprecated Farbe über Request ermitteln
-     *
-     * @return FundImageController
-     */
-    public function blueColor() : FundImageController
-    {
-        $this->_font['color'] = FundImageController::COLORS['blue'];
-        return $this;
+        return $this->_loadImageFromDisk();
     }
 
     private function _checkIfImageCanBeCreated() : void
@@ -132,18 +120,86 @@ class FundImageController extends Controller
         }
     }
 
+    private function _setImageType() : void
+    {
+        if (in_array(Route::getCurrentRoute()->getAction()['type'], FundImageController::SUPPORTED_FUNDS)) {
+            $this->_image['type'] = Route::getCurrentRoute()->getAction()['type'];
+        } else {
+            throw new \InvalidArgumentException('FundImage function only accepts Supported Image Types('
+                .implode(', ',FundImageController::SUPPORTED_FUNDS).'). Input was: '
+                .Route::getCurrentRoute()->getAction()['type']);
+        }
+    }
+
+    private function _assembleFileName() : void
+    {
+        if ($this->_font['color'] === FundImageController::COLORS['black']) {
+            $color = '_black';
+        } else {
+            $color = '_blue';
+        }
+        $this->_image['name'] = $this->_image['type'].$color.'.png';
+    }
+
+    private function _checkIfImageCanBeLoadedFromCache() : bool
+    {
+        if (Storage::disk(FUNDIMAGE_DISK_SAVE_PATH)->exists($this->_image['name']) &&
+            Storage::disk(FUNDIMAGE_DISK_SAVE_PATH)->lastModified($this->_image['name']) > time() - FUNDIMAGE_CACHE_TIME) {
+            return true;
+        }
+        return false;
+    }
+
+    private function _getFundsFromAPI() : void
+    {
+        $funds = $this->_api->lastHours()->getCrowdfundStats()->asArray();
+        $this->_funds['current'] = substr($funds['data']['funds'], 0, -2);
+    }
+
+    private function _formatFunds($source = 'current') : void
+    {
+        if ($source !== 'current') {
+            $source = 'nextMillion';
+        }
+        $this->_funds[$source.'Formatted'] = number_format($this->_funds[$source], 0, ',', '.').' $';
+    }
+
+    private function _determineImageWidth() : void
+    {
+        switch ($this->_image['type']) {
+            case FundImageController::FUNDING_ONLY:
+                $this->_image['width'] = 230;
+                break;
+
+            case FundImageController::FUNDING_AND_TEXT:
+                $this->_image['width'] = 280;
+                break;
+
+            case FundImageController::FUNDING_AND_BARS:
+                $this->_image['width'] = 305;
+                break;
+        }
+    }
+
     private function _determineImageHeight() : void
     {
-        if ($this->_image['type'] === FundImageController::FUNDING_ONLY) {
-            $this->_image['height'] = 35;
-        } else {
-            $this->_image['height'] = 75;
+        switch ($this->_image['type']) {
+            case FundImageController::FUNDING_ONLY:
+                $this->_image['height'] = 35;
+                break;
+
+            case FundImageController::FUNDING_AND_TEXT:
+                $this->_image['height'] = 75;
+                break;
+
+            case FundImageController::FUNDING_AND_BARS:
+                $this->_image['height'] = 41;
+                break;
         }
     }
 
     private function _initImage() : void
     {
-        $this->_image['width'] = 280;
         $this->_image['pointer'] = imagecreatetruecolor( $this->_image['width'], $this->_image['height'] );
         imagesavealpha( $this->_image['pointer'], true );
 
@@ -154,17 +210,65 @@ class FundImageController extends Controller
     private function _addDataToImage() : void
     {
         $fontColor = $this->_makeColorFromArray();
-        if ($this->_image['type'] === FundImageController::FUNDING_AND_TEXT) {
-            imagettftext($this->_image['pointer'], 25, 0, 0, 30, $fontColor, $this->_font['path'], $this->_image['text']);
-            imagettftext($this->_image['pointer'], 25, 0, 2, 70, $fontColor, $this->_font['path'], $this->_funds);
-        } else {
-            imagettftext($this->_image['pointer'], 20, 0, 2, 30, $fontColor, $this->_font['path'], $this->_funds);
+        switch ($this->_image['type']) {
+            case FundImageController::FUNDING_AND_TEXT:
+                imagettftext($this->_image['pointer'], 25, 0, 0, 30, $fontColor, $this->_font['path'], $this->_image['text']);
+                imagettftext($this->_image['pointer'], 25, 0, 2, 70, $fontColor, $this->_font['path'], $this->_funds['currentFormatted']);
+                break;
+
+            case FundImageController::FUNDING_AND_BARS:
+                $this->_initBarImage();
+                $fontColor = $this->_makeColorFromArray();
+                imagestring($this->_image['pointer'], 2, 0, 0, $this->_image['text'], $fontColor);
+                $this->_addBarsToBarImage();
+                break;
+
+            case FundImageController::FUNDING_ONLY:
+                imagettftext($this->_image['pointer'], 20, 0, 2, 30, $fontColor, $this->_font['path'], $this->_funds['currentFormatted']);
+                break;
         }
+    }
+
+    private function _initBarImage() : void
+    {
+        $this->_font['color'] = FundImageController::COLORS['blue'];
+        $this->_roundFundsToNextMillion();
+        $this->_calculatePercentageToNextMillion();
+        $this->_image['text'] = 'Crowdfunding: '.$this->_funds['currentFormatted'].
+                                ' von '.$this->_funds['nextMillionFormatted'].
+                                ' ('.$this->_funds['percentageToNextMillion'].'%)';
+    }
+
+    private function _addBarsToBarImage() : void
+    {
+        $this->_font['color'] = FundImageController::COLORS['darkblue'];
+        $darkBlue = $this->_makeColorFromArray();
+
+        $this->_font['color'] = FundImageController::COLORS['blue'];
+        $blue = $this->_makeColorFromArray();
+
+        for ($i = 0; $i <= 300; $i = $i + 5) {
+            if ((($this->_funds['nextMillion'] - $this->_funds['substractor']) / 1000000) * 100 >= $i) {
+                imageline($this->_image['pointer'], $i, 15, $i, 40, $blue);
+                imageline($this->_image['pointer'], $i + 1, 15, $i + 1, 40, $blue);
+                imageline($this->_image['pointer'], $i + 2, 15, $i + 2, 40, $blue);
+            } else {
+                imageline($this->_image['pointer'], $i, 15, $i, 40, $darkBlue);
+                imageline($this->_image['pointer'], $i + 1, 15, $i + 1, 40, $darkBlue);
+                imageline($this->_image['pointer'], $i + 2, 15, $i + 2, 40, $darkBlue);
+            }
+        }
+
     }
 
     private function _makeColorFromArray()
     {
-        return imagecolorallocate($this->_image['pointer'], $this->_font['color'][0], $this->_font['color'][1], $this->_font['color'][2]);
+        return imagecolorallocate(
+            $this->_image['pointer'],
+            $this->_font['color'][0],
+            $this->_font['color'][1],
+            $this->_font['color'][2]
+        );
     }
 
     private function _flushImageToString() : void
@@ -185,45 +289,20 @@ class FundImageController extends Controller
         return response()->file(storage_path(FUNDIMAGE_RELATIVE_SAVE_PATH.$this->_image['name']));
     }
 
-    private function _assembleFileName() : void
+    private function _roundFundsToNextMillion()
     {
-        if ($this->_font['color'] === FundImageController::COLORS['black']) {
-            $color = '_black';
-        } else {
-            $color = '_blue';
+        $this->_funds['nextMillion'] = ceil($this->_funds['current'] / 1000000) * 1000000;
+        $this->_formatFunds('nextMillion');
+    }
+
+    private function _calculatePercentageToNextMillion()
+    {
+        if ($this->_funds['nextMillion'] === null ||
+            $this->_funds['current'] === null) {
+            throw new InvalidDataException('Did you call _roundFundsToNextMillion()?');
         }
-        $this->_image['name'] = $this->_image['type'].$color.'.png';
+        $this->_funds['substractor'] = $this->_funds['nextMillion'] - 1000000;
+        $this->_funds['percentageToNextMillion'] = round((($this->_funds['current'] - $this->_funds['substractor']) /
+                                                   ($this->_funds['nextMillion'] - $this->_funds['substractor'])) * 100);
     }
-
-    private function _getFundsFromAPI() : void
-    {
-        $funds = $this->_api->lastHours()->getCrowdfundStats()->asArray();
-        $this->_funds = $funds['data']['funds'];
-    }
-
-    private function _formatFunds() : void
-    {
-        $this -> _funds = number_format(substr($this->_funds, 0, -2), 0, ',', '.') . '$';
-    }
-
-	private function _setImageType() : void
-	{
-		if (in_array(Route::getCurrentRoute()->getAction()['type'], FundImageController::SUPPORTED_FUNDS)) {
-			$this->_image['type'] = Route::getCurrentRoute()->getAction()['type'];
-		} else {
-			throw new \InvalidArgumentException('FundImage function only accepts Supported Image Types('
-				.implode(", ",FundImageController::SUPPORTED_FUNDS).'). Input was: '
-				.Route::getCurrentRoute()->getAction()['type']);
-		}
-	}
-
-	private function _checkIfImageCanBeLoadedFromCache() : bool
-    {
-        if (Storage::disk(FUNDIMAGE_DISK_SAVE_PATH)->exists($this->_image['name']) &&
-            Storage::disk(FUNDIMAGE_DISK_SAVE_PATH)->lastModified($this->_image['name']) > time() - FUNDIMAGE_CACHE_TIME) {
-            return true;
-        }
-        return false;
-    }
-
 }
--- tests/FundImageTest.php
+++ tests/FundImageTest.php
@@ -6,14 +6,36 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class FundImageTest extends TestCase
 {
+    private $_gdInstalled = false;
 
-    public function testImageGeneration()
+    public function setUp()
     {
-        if (!in_array('gd', get_loaded_extensions())) {
-            $this->expectException(\App\Exceptions\MissingExtensionException::class);
-            $this->visit('/media/images/funds');
-        } else {
+        parent::setUp();
+        if (in_array('gd', get_loaded_extensions())) {
+            $this->_gdInstalled = true;
+        }
+    }
+
+    public function testFundImage()
+    {
+        if ($this->_gdInstalled) {
             $this->get('/media/images/funds');
+            $this->assertResponseOk();
+        }
+    }
+
+    public function testFundImageWithText()
+    {
+        if ($this->_gdInstalled) {
+            $this->get('/media/images/funds/text');
+            $this->assertResponseOk();
+        }
+    }
+
+    public function testFundImageWithBars()
+    {
+        if ($this->_gdInstalled) {
+            $this->get('/media/images/funds/bar');
             $this->assertResponseOk();
         }
     }
