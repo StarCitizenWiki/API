e0437c6839cba99654a4c0ceab9b1896e664a127
FoXFTW <hannes@octofox.de>
2017-02-01 10:55:24 +0100

    Fix missing type Parameter

--- app/constants.php
+++ app/constants.php
@@ -4,6 +4,7 @@
  * Global Constants
  */
 
+/** FundImageController */
 define('FUNDIMAGE_FUNDING_ONLY',  'funding_only');
 define('FUNDIMAGE_FUNDING_AND_TEXT',  'funding_and_text');
 define('FUNDIMAGE_FUNDING_AND_BARS',  'funding_and_bars');
