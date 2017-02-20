85e6ff1a91fdabb97f5d7143ee466245e46fdfb5
FoXFTW <hannes@octofox.de>
2017-02-20 21:54:40 +0100

    Add X-Rsi-Token as default header

--- app/Repositories/StarCitizen/APIv1/BaseStarCitizenAPI.php
+++ app/Repositories/StarCitizen/APIv1/BaseStarCitizenAPI.php
@@ -9,12 +9,22 @@ namespace App\Repositories\StarCitizen\APIv1;
 
 use App\Exceptions\InvalidDataException;
 use App\Repositories\BaseAPI;
+use GuzzleHttp\Client;
 
 class BaseStarCitizenAPI
 {
     const API_URL = 'https://robertsspaceindustries.com/api/';
 
     use BaseAPI;
+
+    function __construct()
+    {
+        $this->_connection = new Client([
+            'base_uri' => $this::API_URL,
+            'timeout' => 3.0,
+            'headers' => ['X-Rsi-Token' => null]
+        ]);
+    }
 
     private function _checkIfResponseIsValid()
     {
