7308d8b8c9dc11199cef9ed4ddb84ce385ea6b09
FoXFTW <hannes@octofox.de>
2017-04-04 16:58:53 +0200

    Test: Add xdebug and gd to docker php image

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,10 +7,12 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git unzip -yqq
+apt-get install git unzip libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng12-dev -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 
 docker-php-ext-install gd
+pecl install xdebug
+docker-php-ext-enable xdebug
