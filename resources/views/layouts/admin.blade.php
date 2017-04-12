6ee0eb5ec9fce5f75c100c17346161e615b230bf
FoXFTW <hannes@octofox.de>
2017-04-12 15:00:01 +0200

    Collapse Admin Groups in Menu

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,6 +19,11 @@
             'csrfToken' => csrf_token(),
         ]) !!};
         </script>
+        <style>
+            a.collapsed i::before {
+                content: "\f0da" !important;
+            }
+        </style>
         @yield('header')
     </head>
     <body style="min-height: 100vh;">
@@ -26,90 +31,104 @@
         <div class="container-fluid" style="min-height: 100vh;">
             <div class="row" style="min-height: 100vh;">
                 <div class="col-12 col-md-2 bg-inverse pb-4" style="min-height: 100vh;">
-                    <img src="{{ asset('media/images/Star_Citizen_Wiki_Logo.png') }}" class="d-block mx-auto my-4 img-fluid" style="max-width: 100px;">
+                    <a href="{{ route('admin_dashboard') }}">
+                        <img src="{{ asset('media/images/Star_Citizen_Wiki_Logo.png') }}" class="d-block mx-auto my-4 img-fluid" style="max-width: 100px;">
+                    </a>
                     <ul class="nav flex-column">
                         <li class="nav-item ">
                             <span class="nav-link text-muted">App</span>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white active" href="{{ route('auth_logout') }}"><i class="fa fa-sign-out mr-1"></i> Logout</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white active" href="{{ route('admin_logs') }}"><i class="fa fa-book mr-1"></i> Logs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white active" href="//{{ config('app.api_url') }}"><i class="fa fa-cogs mr-1"></i> API</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link text-white" href="//{{ config('app.tools_url') }}"><i class="fa fa-wrench mr-1"></i> Tools</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="//{{ config('app.shorturl_url') }}"><i class="fa fa-link mr-1"></i> ShortURL</a>
-                        </li>
-                    </ul>
-                    <ul class="nav flex-column mt-4">
-                        <li class="nav-item ">
-                            <span class="nav-link text-muted">Admin</span>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white active" href="{{ route('admin_dashboard') }}"><i class="fa fa-dashboard mr-1"></i> Dashboard</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link text-white" href="{{ route('admin_routes_list') }}"><i class="fa fa-random mr-1"></i> Routes</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('admin_users_list') }}"><i class="fa fa-users mr-1"></i> User</a>
+                            <ul class="nax flex-column pl-0" id="app">
+                                @unless(is_null(\Illuminate\Support\Facades\Auth::user()))
+                                <li class="nav-item">
+                                    <a class="nav-link text-white active" href="{{ route('auth_logout') }}"><i class="fa fa-sign-out mr-1"></i> Logout</a>
+                                </li>
+                                @endif
+                                <li class="nav-item">
+                                    <a class="nav-link text-white active" href="{{ route('admin_logs') }}"><i class="fa fa-book mr-1"></i> Logs</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white active" href="//{{ config('app.api_url') }}"><i class="fa fa-cogs mr-1"></i> API</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link text-white" href="//{{ config('app.tools_url') }}"><i class="fa fa-wrench mr-1"></i> Tools</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="//{{ config('app.shorturl_url') }}"><i class="fa fa-link mr-1"></i> ShortURL</a>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
                     <ul class="nav flex-column mt-4">
                         <li class="nav-item ">
-                            <span class="nav-link text-muted">URLs</span>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link text-white" href="{{ route('admin_urls_list') }}"><i class="fa fa-link mr-1"></i> ShortURLs</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link text-white" href="{{ route('admin_urls_whitelist_list') }}"><i class="fa fa-list mr-1"></i> Whitelist</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link text-white" href="{{ route('admin_urls_whitelist_add_form') }}"><i class="fa fa-plus-circle mr-1"></i> Add Whitelist</a>
-                        </li>
-                    </ul>
-                    <ul class="nav flex-column mt-4">
-                        <li class="nav-item ">
-                            <span class="nav-link text-muted">Starmap</span>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link text-white" href="{{ route('admin_starmap_systems_list') }}"><i class="fa fa-circle-o-notch mr-1"></i> Systems</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link text-white" href="{{ route('admin_starmap_systems_add_form') }}"><i class="fa fa-plus-circle mr-1"></i> Add System</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a href="#" class="nav-link text-white" onclick="event.preventDefault(); document.getElementById('download-starmap').submit();">
-                                <form id="download-starmap" action="{{ route('admin_starmap_systems_download') }}" method="POST" style="display: none;">
-                                    <input name="_method" type="hidden" value="POST">
-                                    {{ csrf_field() }}
-                                </form>
-                                <i class="fa fa-repeat mr-1"></i> Download Starmap
-                            </a>
+                            <a class="nav-link text-muted collapsed" data-toggle="collapse" href="#admin" aria-expanded="false" aria-controls="admin">Admin <i class="fa fa-caret-down ml-2"></i> </a>
+                            <ul class="nax flex-column collapse pl-0" id="admin">
+                                <li class="nav-item">
+                                    <a class="nav-link text-white active" href="{{ route('admin_dashboard') }}"><i class="fa fa-dashboard mr-1"></i> Dashboard</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link text-white" href="{{ route('admin_routes_list') }}"><i class="fa fa-random mr-1"></i> Routes</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="{{ route('admin_users_list') }}"><i class="fa fa-users mr-1"></i> User</a>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
                     <ul class="nav flex-column mt-4">
                         <li class="nav-item ">
-                            <span class="nav-link text-muted">Ships</span>
+                            <a class="nav-link text-muted collapsed" data-toggle="collapse" href="#urls" aria-expanded="false" aria-controls="urls">URLs <i class="fa fa-caret-down ml-2"></i> </a>
+                            <ul class="nax flex-column collapse pl-0" id="urls">
+                                <li class="nav-item ">
+                                    <a class="nav-link text-white" href="{{ route('admin_urls_list') }}"><i class="fa fa-link mr-1"></i> ShortURLs</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link text-white" href="{{ route('admin_urls_whitelist_list') }}"><i class="fa fa-list mr-1"></i> Whitelist</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link text-white" href="{{ route('admin_urls_whitelist_add_form') }}"><i class="fa fa-plus-circle mr-1"></i> Add Whitelist</a>
+                                </li>
+                            </ul>
                         </li>
+                    </ul>
+                    <ul class="nav flex-column mt-4">
                         <li class="nav-item ">
-                            <a class="nav-link text-white" href="{{ route('admin_ships_list') }}"><i class="fa fa-rocket mr-1"></i> Ships</a>
+                            <a class="nav-link text-muted collapsed" data-toggle="collapse" href="#starmap" aria-expanded="false" aria-controls="starmap">Starmap <i class="fa fa-caret-down ml-2"></i> </a>
+                            <ul class="nax flex-column collapse pl-0" id="starmap">
+                                <li class="nav-item ">
+                                    <a class="nav-link text-white" href="{{ route('admin_starmap_systems_list') }}"><i class="fa fa-circle-o-notch mr-1"></i> Systems</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link text-white" href="{{ route('admin_starmap_systems_add_form') }}"><i class="fa fa-plus-circle mr-1"></i> Add System</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a href="#" class="nav-link text-white" onclick="event.preventDefault(); document.getElementById('download-starmap').submit();">
+                                        <form id="download-starmap" action="{{ route('admin_starmap_systems_download') }}" method="POST" style="display: none;">
+                                            <input name="_method" type="hidden" value="POST">
+                                            {{ csrf_field() }}
+                                        </form>
+                                        <i class="fa fa-repeat mr-1"></i> Download Starmap
+                                    </a>
+                                </li>
+                            </ul>
                         </li>
+                    </ul>
+                    <ul class="nav flex-column mt-4">
                         <li class="nav-item ">
-                            <a href="#" class="nav-link text-white" onclick="event.preventDefault(); document.getElementById('download-ships').submit();">
-                                <form id="download-ships" action="{{ route('admin_ships_download') }}" method="POST" style="display: none;">
-                                    <input name="_method" type="hidden" value="POST">
-                                    {{ csrf_field() }}
-                                </form>
-                                <i class="fa fa-repeat mr-1"></i> Download Ships
-                            </a>
+                            <a class="nav-link text-muted collapsed" data-toggle="collapse" href="#ships" aria-expanded="false" aria-controls="ships">Ships <i class="fa fa-caret-down ml-2"></i> </a>
+                            <ul class="nax flex-column collapse pl-0" id="ships">
+                                <li class="nav-item ">
+                                    <a class="nav-link text-white" href="{{ route('admin_ships_list') }}"><i class="fa fa-rocket mr-1"></i> Ships</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a href="#" class="nav-link text-white" onclick="event.preventDefault(); document.getElementById('download-ships').submit();">
+                                        <form id="download-ships" action="{{ route('admin_ships_download') }}" method="POST" style="display: none;">
+                                            <input name="_method" type="hidden" value="POST">
+                                            {{ csrf_field() }}
+                                        </form>
+                                        <i class="fa fa-repeat mr-1"></i> Download Ships
+                                    </a>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
                 </div>
