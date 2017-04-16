b7602ebc714235836b256e4a33dec5787e028d73
FoXFTW <hannes@octofox.de>
2017-04-16 14:56:11 +0200

    Dispatch SplitShipFiles after Download
    Add $skip array with filenames to skip on download

--- app/Jobs/DownloadStarCitizenDBShips.php
+++ app/Jobs/DownloadStarCitizenDBShips.php
@@ -21,10 +21,14 @@ class DownloadStarCitizenDBShips implements ShouldQueue
 
     const STARCITIZENDB_URL = 'http://starcitizendb.com/';
 
+    private $skip = [
+        'RSI_Constellation_SCItemTest.json',
+        'MITE_SimPod.json',
+        'GRIN_PTV.json',
+    ];
+
     /**
      * Create a new job instance.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -49,15 +53,19 @@ class DownloadStarCitizenDBShips implements ShouldQueue
         foreach ($urls as $url) {
             $fileName = explode('/', $url);
             $fileName = end($fileName);
-            $resource = fopen(config('filesystems.disks.scdb_ships.root').'/'.$fileName, 'w');
-            $stream = stream_for($resource);
-            $client->request(
-                'GET',
-                DownloadStarCitizenDBShips::STARCITIZENDB_URL.$url,
-                ['save_to' => $stream]
-            );
-            Log::info('Downloading '.$fileName);
+            if (!in_array($fileName, $this->skip)) {
+                $resource = fopen(config('filesystems.disks.scdb_ships.root').'/'.$fileName, 'w');
+                $stream = stream_for($resource);
+                $client->request(
+                    'GET',
+                    DownloadStarCitizenDBShips::STARCITIZENDB_URL.$url,
+                    ['save_to' => $stream]
+                );
+                Log::info('Downloading '.$fileName);
+            }
         }
         Log::info('Ship Download Job Finished');
+        Log::info('Dispatching Split Files Job');
+        dispatch(new SplitShipFiles());
     }
 }
