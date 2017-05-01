272448380b661b711cc373ddec7206b6a2facd4c
FoXFTW <hannes@octofox.de>
2017-05-02 00:03:48 +0200

    :hammer: Refactor AsteroidbeltsTransformer
    :pencil2: astereoids -> asteroids
    :pencil2: celestrial -> celestial
    Tabs -> Spaces

--- app/Transformers/StarCitizen/Starmap/AsteroidbeltsTransformer.php
+++ app/Transformers/StarCitizen/Starmap/AsteroidbeltsTransformer.php
@@ -17,7 +17,7 @@ use League\Fractal\TransformerAbstract;
  */
 class AsteroidbeltsTransformer extends TransformerAbstract implements BaseAPITransformerInterface
 {
-	private const OBJECT_TYPE = 'ASTEROID_BELT';
+    private const OBJECT_TYPE = 'ASTEROID_BELT';
 
     /**
      * Returns all Asteroidbelts of the System Data
@@ -28,37 +28,35 @@ class AsteroidbeltsTransformer extends TransformerAbstract implements BaseAPITra
      */
     public function transform($system)
     {
-	    $transformed = [
-		    'success' => $system['success'],
-		    'rowcount'  => $system['data']['rowcount'],
-		    'totalrows'  => $system['data']['totalrows'],
-		    'estimatedrows'  => $system['data']['estimatedrows'],
-		    'pagesize'  => $system['data']['pagesize'],
-		    'pagecount'  => $system['data']['pagecount'],
-		    'page'  => $system['data']['page'],
-		    'offset'  => $system['data']['offset'],
-		    'startrow'  => $system['data']['startrow']
-	    ];
+        $transformed = [
+            'success' => $system['success'],
+            'rowcount'  => $system['data']['rowcount'],
+            'totalrows'  => $system['data']['totalrows'],
+            'estimatedrows'  => $system['data']['estimatedrows'],
+            'pagesize'  => $system['data']['pagesize'],
+            'pagecount'  => $system['data']['pagecount'],
+            'page'  => $system['data']['page'],
+            'offset'  => $system['data']['offset'],
+            'startrow'  => $system['data']['startrow'],
+        ];
 
-	    $asteroidbelts = [];
-	    $celestrialObjects = $system['data']['resultset'][0]['celestial_objects'];
-	    foreach($celestrialObjects as $celestrialObject)
-	    {
-		    if (strcmp($celestrialObject['type'], self::OBJECT_TYPE) == 0)
-		    {
-			    $asteroidbelts[] = $celestrialObject;
-		    }
-	    }
+        $asteroidbelts = [];
+        $celestialObjects = $system['data']['resultset'][0]['celestial_objects'];
+        foreach ($celestialObjects as $celestialObject) {
+            if (strcmp($celestialObject['type'], self::OBJECT_TYPE) == 0) {
+                $asteroidbelts[] = $celestialObject;
+            }
+        }
 
-	    $transformedAstereoidbelts = [
-		    'resultset' => $asteroidbelts
-	    ];
+        $transformedAsteroidbelts = [
+            'resultset' => $asteroidbelts,
+        ];
 
-	    $codeMessage = [
-		    'code'  => $system['code'],
-		    'msg'  => $system['msg']
-	    ];
+        $codeMessage = [
+            'code'  => $system['code'],
+            'msg'  => $system['msg'],
+        ];
 
-	    return array_merge($transformed, $transformedAstereoidbelts, $codeMessage);
+        return array_merge($transformed, $transformedAsteroidbelts, $codeMessage);
     }
 }
