c6b33a67d50b33594a57e37a4ddb7feb98365d6d
FoXFTW <hannes@octofox.de>
2017-05-02 14:19:21 +0200

    :rotating_light: Remove unused constructors from Exceptions

--- app/Exceptions/ExpiredException.php
+++ app/Exceptions/ExpiredException.php
@@ -17,15 +17,5 @@ use Exception;
  */
 class ExpiredException extends Exception
 {
-    /**
-     * ExpiredException constructor.
-     *
-     * @param string         $message  Message
-     * @param int            $code     Code
-     * @param Exception|null $previous Exception
-     */
-    public function __construct($message = "", $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
+
 }
--- app/Exceptions/HashNameAlreadyAssignedException.php
+++ app/Exceptions/HashNameAlreadyAssignedException.php
@@ -17,15 +17,5 @@ use Exception;
  */
 class HashNameAlreadyAssignedException extends Exception
 {
-    /**
-     * HashNameAlreadyAssignedException constructor.
-     *
-     * @param string         $message  Message
-     * @param int            $code     Code
-     * @param Exception|null $previous Exception
-     */
-    public function __construct($message = "", $code = 0, Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
+
 }
