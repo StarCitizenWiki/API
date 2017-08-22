8f5da3942edfc2d0492a3040df07b73e18d392d7
FoXFTW <hannes@octofox.de>
2017-08-22 23:06:46 +0200

    :lipstick: Add restorebutton to edit_delete_block.blade.php

--- resources/views/components/edit_delete_block.blade.php
+++ resources/views/components/edit_delete_block.blade.php
@@ -1,17 +1,36 @@
 <div class="btn-group btn-group-sm" role="group" aria-label="">
     @unless(empty($edit_url))
-    <a href="{{ $edit_url or '' }}" class="btn btn-outline-secondary">
-        <i class="fa fa-pencil"></i>
-    </a>
+        <a href="{{ $edit_url or '' }}" class="btn btn-outline-secondary">
+            @component('components.elements.icon')
+                pencil
+            @endcomponent
+        </a>
     @endunless
     @unless(empty($delete_url))
-    <a href="#" class="btn btn-outline-danger" onclick="event.preventDefault(); document.getElementById('delete-form{{ $slot }}').submit();">
-        <form id="delete-form{{ $slot }}" action="{{ $delete_url or '' }}" method="POST" style="display: none;">
-            <input name="_method" type="hidden" value="DELETE">
-            <input name="id" type="hidden" value="{{ $slot }}">
-            {{ csrf_field() }}
-        </form>
-        <i class="fa fa-trash-o"></i>
-    </a>
+        <a href="#" class="btn btn-outline-danger" onclick="event.preventDefault(); document.getElementById('delete-form{{ $slot }}').submit();">
+            @component('components.forms.form', [
+                'id' => 'delete-form'.$slot,
+                'class' => 'd-none',
+                'action' => $delete_url,
+                'method' => 'DELETE',
+            ])@endcomponent
+            @component('components.elements.icon')
+                trash-o
+            @endcomponent
+        </a>
+    @endunless
+    @unless(empty($restore_url))
+        <a href="#" class="btn btn-outline-success" onclick="event.preventDefault(); document.getElementById('restore-form{{ $slot }}').submit();">
+            @component('components.forms.form', [
+                'id' => 'restore-form'.$slot,
+                'class' => 'd-none',
+                'action' => $restore_url,
+            ])@endcomponent
+            @component('components.elements.icon', [
+                'class' => 'fa-flip-horizontal',
+            ])
+                repeat
+            @endcomponent
+        </a>
     @endunless
 </div>
