2a3ffd016f324efeb0da60ef5d1527d4567129dd
FoXFTW <hannes@octofox.de>
2018-03-23 14:34:13 +0100

    :construction: Add Ship Model and Migration

---
+++ app/Models/StarCitizen/Ship.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Ship
+ */
+class Ship extends Model
+{
+    //
+}
---
+++ database/migrations/api/starcitizen/2018_03_23_142835_create_ships_table.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ships', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ships');
+    }
+}
