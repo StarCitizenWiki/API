7fb37e507e475390d8229b7d5ba206cb2e9ed854
FoXFTW <hannes@octofox.de>
2018-07-25 21:13:57 +0200

    :memo: Add Api Documentation

--- app/Http/Controllers/Api/V1/StarCitizen/Vehicle/Ship/ShipController.php
+++ app/Http/Controllers/Api/V1/StarCitizen/Vehicle/Ship/ShipController.php
@@ -92,7 +92,7 @@ class ShipController extends Controller
      *          }
      *      }
      * })
-     * @Response(404)
+     * @Response(404, body={"message": "No Ship found for Query: Ship Name", "status_code": 404})
      */
     public function show(string $shipName)
     {
@@ -108,6 +108,81 @@ class ShipController extends Controller
      *
      * @Parameters({
      *      @Parameter("page", description="The page of results to view.", default=1)
+     * })
+     *
+     * @Response(200, body={
+     *     "data": {
+     *          {
+     *              "id": 7,
+     *              "chassis_id": 2,
+     *              "name": "300i",
+     *              "sizes": {
+     *                  "length": 23,
+     *                  "beam": 15.5,
+     *                  "height": 7
+     *              },
+     *              "mass": 65925,
+     *              "cargo_capacity": 2,
+     *              "crew": {
+     *                  "min": 1,
+     *                  "max": 1
+     *              },
+     *              "speed": {
+     *                  "scm": 275,
+     *                  "afterburner": 1190
+     *              },
+     *              "rotation": {
+     *                  "pitch": 85,
+     *                  "yaw": 85,
+     *                  "roll": 120
+     *              },
+     *              "acceleration": {
+     *                  "x_axis": 68,
+     *                  "y_axis": 80.3,
+     *                  "z_axis": 71.7
+     *              },
+     *              "foci": {
+     *                  {
+     *                      "de_DE": "Reisen",
+     *                      "en_EN": "Touring"
+     *                  }
+     *              ],
+     *              "production_status": {
+     *                  "de_DE": "Flugbereit",
+     *                  "en_EN": "flight-ready"
+     *              },
+     *              "type": {
+     *                  "de_DE": "Erkundung",
+     *                  "en_EN": "exploration"
+     *              },
+     *              "description": {
+     *                  "de_DE": "[...]",
+     *                  "en_EN": "If you're going to travel the stars... [...]"
+     *              },
+     *              "size": {
+     *                  "de_DE": "Klein",
+     *                  "en_EN": "small"
+     *              },
+     *              "manufacturer": {
+     *                  "code": "ORIG",
+     *                  "name": "Origin Jumpworks GmbH"
+     *              }
+     *          },
+     *          {}
+     *     },
+     *     "meta": {
+     *          "pagination": {
+     *              "total": 1000,
+     *              "count": 15,
+     *              "per_page": 15,
+     *              "current_page": 1,
+     *              "total_pages": 100,
+     *              "links": {
+     *                  "next": "Link",
+     *                  "prev": "Link"
+     *              }
+     *          }
+     *     }
      * })
      */
     public function index()
