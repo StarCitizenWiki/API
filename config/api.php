6c3d360c7e73210aa1161b8cbb4cf48ff65b909c
FoXFTW <hannes@octofox.de>
2018-08-01 21:50:12 +0200

    :sparkles: Implement Custom Api Throttle

--- config/api.php
+++ config/api.php
@@ -168,7 +168,7 @@ return [
     */
 
     'auth' => [
-
+        'custom' => \App\Providers\DingoTokenAuthProvider::class,
     ],
 
     /*
@@ -183,7 +183,12 @@ return [
     */
 
     'throttling' => [
-
+        'custom' => new \App\Http\Throttle\ApiThrottle(
+            [
+                'limit' => env('THROTTLE_GUEST_REQUESTS', 10),
+                'expires' => env('THROTTLE_PERIOD', 1),
+            ]
+        ),
     ],
 
     /*
@@ -229,38 +234,24 @@ return [
 
     ],
 
-
-
-
     'wiki_url' => env('WIKI_URL', 'http://localhost'),
     'wiki_query_token' => env('WIKI_QUERY_TOKEN', null),
     'rsi_url' => env('RSI_URL', 'https://robertsspaceindustries.com'),
 
     'admin_password' => env('ADMIN_PASSWORD', config('app.key')),
 
-    'throttle' => [
-        'period'         => env('THROTTLE_PERIOD', 1),
-        'guest_requests' => env('THROTTLE_GUEST_REQUESTS', 10),
-    ],
-
     'log' => [
-        'error'   => [
-            'danger_hour'  => 10,
-            'danger_day'   => 20,
+        'error' => [
+            'danger_hour' => 10,
+            'danger_day' => 20,
             'warning_hour' => 5,
-            'warning_day'  => 10,
+            'warning_day' => 10,
         ],
         'warning' => [
-            'danger_hour'  => 20,
-            'danger_day'   => 40,
+            'danger_hour' => 20,
+            'danger_day' => 40,
             'warning_hour' => 10,
-            'warning_day'  => 20,
+            'warning_day' => 20,
         ],
-    ],
-
-    'transform_types' => [
-        'collection' => 'collection',
-        'item'       => 'item',
-        'null'       => 'NullResource',
     ],
 ];
