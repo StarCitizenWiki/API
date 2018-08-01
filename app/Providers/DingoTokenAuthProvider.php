052b6e9fe2fb0f77824276280d3b60c887f15088
FoXFTW <hannes@octofox.de>
2018-08-01 21:49:30 +0200

    :sparkles: Implement custom Token Auth Provider for Dingo

---
+++ app/Providers/DingoTokenAuthProvider.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types = 1);
+/**
+ * User: Hannes
+ * Date: 01.08.2018
+ * Time: 20:40
+ */
+
+namespace App\Providers;
+
+use App\Models\Account\User\User;
+use Dingo\Api\Auth\Provider\Authorization;
+use Dingo\Api\Routing\Route;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+/**
+ * Simple Auth Provider vor Tokens
+ */
+class DingoTokenAuthProvider extends Authorization
+{
+    private const API_TOKEN = 'api_token';
+
+    /**
+     * Get the providers authorization method.
+     *
+     * @return string
+     */
+    public function getAuthorizationMethod()
+    {
+        return 'bearer';
+    }
+
+    /**
+     * Authenticate the request and return the authenticated user instance.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Dingo\Api\Routing\Route $route
+     *
+     * @return \App\Models\Account\User\User
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
+     */
+    public function authenticate(Request $request, Route $route)
+    {
+        if (!$request->has(self::API_TOKEN)) {
+            $this->validateAuthorizationHeader($request);
+        }
+
+        $user = User::where(self::API_TOKEN, $request->bearerToken() ?? $request->get(self::API_TOKEN))->first();
+
+        if (null === $user) {
+            throw new UnauthorizedHttpException($this->getAuthorizationMethod());
+        }
+
+        return $user;
+    }
+}
