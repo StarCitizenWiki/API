b1f293ea27766440610b8ff6cf85f526cf4f1501
FoXFTW <hannes@octofox.de>
2018-08-14 12:25:12 +0200

    :white_check_mark: Add Admin Star Citizen Tests

---
+++ tests/Feature/Controller/Admin/StarCitizen/Vehicle/Size/SizeControllerTestCase.php
@@ -0,0 +1,144 @@
+<?php declare(strict_types = 1);
+/**
+ * Created by PhpStorm.
+ * User: Hanne
+ * Date: 12.08.2018
+ * Time: 16:22
+ */
+
+namespace Tests\Feature\Controller\Admin\StarCitizen\Vehicle\Size;
+
+use App\Models\Api\StarCitizen\Vehicle\Size\VehicleSize;
+use App\Models\Api\StarCitizen\Vehicle\Size\VehicleSizeTranslation;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Tests\Feature\Controller\Admin\StarCitizen\StarCitizenTestCase;
+
+/**
+ * Admin Vehicle Size Controller Test Case
+ */
+class SizeControllerTestCase extends StarCitizenTestCase
+{
+    /**
+     * Index Tests
+     */
+
+    /**
+     * Test Index
+     *
+     * @covers \App\Http\Controllers\Web\Admin\StarCitizen\Vehicle\Size\VehicleSizeController::index
+     */
+    public function testIndex()
+    {
+        $response = $this->actingAs($this->admin, 'admin')->get(route('web.admin.starcitizen.vehicles.sizes.index'));
+        $response->assertStatus(static::RESPONSE_STATUSES['index']);
+
+        if ($response->status() === Response::HTTP_OK) {
+            $response->assertDontSee(__('Keine Übersetzungen vorhanden'));
+        }
+    }
+
+
+    /**
+     * Edit Tests
+     */
+
+    /**
+     * Test Edit
+     *
+     * @covers \App\Http\Controllers\Web\Admin\StarCitizen\Vehicle\Size\VehicleSizeController::edit
+     */
+    public function testEdit()
+    {
+        /** @var \App\Models\Api\StarCitizen\Vehicle\Size\VehicleSize $vehicleSize */
+        $vehicleSize = factory(VehicleSize::class)->create();
+        $vehicleSize->translations()->save(factory(VehicleSizeTranslation::class)->make());
+
+        $response = $this->actingAs($this->admin, 'admin')->get(
+            route('web.admin.starcitizen.vehicles.sizes.edit', $vehicleSize->getRouteKey())
+        );
+        $response->assertStatus(static::RESPONSE_STATUSES['edit']);
+
+        if ($response->status() === Response::HTTP_OK) {
+            $response->assertDontSee(__('Keine Übersetzungen vorhanden'));
+        }
+    }
+
+    /**
+     * Test Edit
+     *
+     * @covers \App\Http\Controllers\Web\Admin\StarCitizen\Vehicle\Size\VehicleSizeController::edit
+     */
+    public function testEditNotFound()
+    {
+        $response = $this->actingAs($this->admin, 'admin')->get(
+            route('web.admin.starcitizen.vehicles.sizes.edit', static::MODEL_ID_NOT_EXISTENT)
+        );
+        $response->assertStatus(static::RESPONSE_STATUSES['edit_not_found']);
+    }
+
+
+    /**
+     * Update Tests
+     */
+
+    /**
+     * Test Update
+     *
+     * @covers \App\Http\Controllers\Web\Admin\StarCitizen\Vehicle\Size\VehicleSizeController::update
+     *
+     * @covers \App\Http\Requests\TranslationRequest
+     *
+     * @covers \App\Models\Api\ModelChangelog
+     */
+    public function testUpdate()
+    {
+        /** @var \App\Models\Api\StarCitizen\Vehicle\Size\VehicleSize $vehicleSize */
+        $vehicleSize = factory(VehicleSize::class)->create();
+        $vehicleSize->translations()->save(factory(VehicleSizeTranslation::class)->make());
+
+        $response = $this->actingAs($this->admin, 'admin')->patch(
+            route('web.admin.starcitizen.vehicles.sizes.update', $vehicleSize->getRouteKey()),
+            [
+                'en_EN' => 'Vehicle Size translation',
+            ]
+        );
+
+        $this->assertNotEquals(ValidationException::class, get_class($response->exception ?? new \stdClass()));
+
+        $response->assertStatus(static::RESPONSE_STATUSES['update']);
+    }
+
+    /**
+     * Test Update
+     *
+     * @covers \App\Http\Controllers\Web\Admin\StarCitizen\Vehicle\Size\VehicleSizeController::update
+     */
+    public function testUpdateNotFound()
+    {
+        $response = $this->actingAs($this->admin, 'admin')->patch(
+            route('web.admin.starcitizen.vehicles.sizes.update', static::MODEL_ID_NOT_EXISTENT),
+            [
+                'en_EN' => 'Vehicle Size translation',
+            ]
+        );
+
+        $this->assertNotEquals(ValidationException::class, get_class($response->exception ?? new \stdClass()));
+
+        $response->assertStatus(static::RESPONSE_STATUSES['update_not_found']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * Creates needed Vehicle sizes
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        factory(VehicleSize::class, 10)->create()->each(
+            function (VehicleSize $vehicleSize) {
+                $vehicleSize->translations()->save(factory(VehicleSizeTranslation::class)->make());
+            }
+        );
+    }
+}
