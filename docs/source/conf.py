f1e49a87b827a058f98704449c3b1641ec230dee
FoXFTW <hannes@octofox.de>
2018-08-28 19:10:55 +0200

    :bento: Add Favicon to Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,6 +88,7 @@ html_theme_options = {
 }
 
 html_logo = "_static/Star_Citizen_Wiki_Logo_White.png"
+html_favicon = "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
