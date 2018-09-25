aadaa82eadd753f733da7a4a3b769bc8c0af4834
FoXFTW <hannes@octofox.de>
2018-09-25 18:09:25 +0200

    :card_file_box: Add Crowdfund Stats Dump | Add Vehicle Translation Dumps

--- database/seeds/api/StatTableSeeder.php
+++ database/seeds/api/StatTableSeeder.php
@@ -14,6 +14,7 @@ class StatTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::unprepared(file_get_contents(database_path('dumps/crowdfundstats_2012-11-13_-_2018-03-15.sql')));
+        DB::unprepared(file_get_contents(database_path('dumps/crowdfundstats/crowdfundstats_2012-11-13_-_2018-03-16.sql')));
+        DB::unprepared(file_get_contents(database_path('dumps/crowdfundstats/crowdfundstats_2018-03-17_-_2018-09-24.sql')));
     }
 }
