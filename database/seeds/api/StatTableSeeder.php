837d9a3bd91600568faa0e3989d49e9831c8380b
FoXFTW <hannes@octofox.de>
2018-09-27 19:06:34 +0200

    :card_file_box: Add Missing Crowdfundstats

--- database/seeds/api/StatTableSeeder.php
+++ database/seeds/api/StatTableSeeder.php
@@ -16,5 +16,6 @@ class StatTableSeeder extends Seeder
     {
         DB::unprepared(file_get_contents(database_path('dumps/crowdfundstats/crowdfundstats_2012-11-13_-_2018-03-16.sql')));
         DB::unprepared(file_get_contents(database_path('dumps/crowdfundstats/crowdfundstats_2018-03-17_-_2018-09-24.sql')));
+        DB::unprepared(file_get_contents(database_path('dumps/crowdfundstats/crowdfundstats_2018-03-25_-_2018-09-26.sql')));
     }
 }
