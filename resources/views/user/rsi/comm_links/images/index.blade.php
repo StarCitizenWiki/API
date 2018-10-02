d15806b6ed02f1b1f5d2f925b1c854f8abdf1848
FoXFTW <hannes@octofox.de>
2018-10-02 14:06:46 +0200

    :recycle: :lipstick: Refactor Comm Link Image Card

--- resources/views/user/rsi/comm_links/images/index.blade.php
+++ resources/views/user/rsi/comm_links/images/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <h3>Comm Link Bilder</h3>
-    <div class="card-columns">
+    <div class="card-columns" style="column-count: 5">
         @foreach($images as $image)
             <div class="card">
                 @if($image->local)
@@ -12,15 +12,25 @@
                 @else
                     <a href="{{ $image->url }}" target="_blank" class="text-center d-block">
                 @endif
-                    <img src="{{ str_replace('source', 'post', $image->src) }}" alt="{{ $image->alt ?? __('Kein alt Text verfügbar') }}">
+                    <img src="{{ config('api.rsi_url') }}{{ str_replace('source', 'post', $image->src) }}" alt="{{ $image->alt ?? __('Kein alt Text verfügbar') }}" class="card-img-top">
                 </a>
-                <ul class="list-group list-group-flush">
+                @unless(empty($image->alt))
+                <div class="card-body">
+                    Bildbeschreibung: {{ $image->alt }}
+                </div>
+                @endunless
+                <ul class="list-group list-group-flush collapse" id="comm_link_container_{{ $loop->index }}">
                     @foreach($image->commLinks as $commLink)
                         <li class="list-group-item">
                             <a href="{{ route('web.user.rsi.comm-links.show', $commLink->getRouteKey()) }}" class="card-link">{{ $commLink->cig_id }} &mdash; {{ $commLink->title }}</a>
                         </li>
                     @endforeach
                 </ul>
+                <div class="card-footer">
+                    <a data-toggle="collapse" href="#comm_link_container_{{ $loop->index }}" role="button" aria-expanded="false" aria-controls="comm_link_container_{{ $loop->index }}">
+                        @lang('Comm Links ausklappen')
+                    </a>
+                </div>
             </div>
         @endforeach
     </div>
