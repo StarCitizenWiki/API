2aa4e0f688e31e5222459d55e796536e295860da
FoXFTW <hannes@octofox.de>
2018-10-03 16:28:16 +0200

    :lipstick: Show RSI Source on Comm Link Images

--- resources/views/user/rsi/comm_links/images/index.blade.php
+++ resources/views/user/rsi/comm_links/images/index.blade.php
@@ -8,7 +8,7 @@
         @foreach($images as $image)
             <div class="card">
                 <a href="{{ $image->getLocalOrRemoteUrl() }}" target="_blank" class="text-center d-block">
-                    <img src="{{ config('api.rsi_url') }}{{ str_replace('source', 'post', $image->src) }}" alt="{{ $image->alt ?? __('Kein alt Text verfügbar') }}" class="card-img-top">
+                    <img src="{{ config('api.rsi_url') }}{{ str_replace('source', 'post', $image->src) }}" alt="{{ empty($image->alt) ? __('Kein alt Text verfügbar') : $image->alt }}" class="card-img-top">
                 </a>
                 @unless(empty($image->alt))
                 <div class="card-body">
@@ -16,6 +16,9 @@
                 </div>
                 @endunless
                 <ul class="list-group list-group-flush collapse" id="comm_link_container_{{ $loop->index }}">
+                    <li class="list-group-item">
+                        <small>Quelle: <a href="{{ $image->url }}" target="_blank">{{ $image->src }}</a></small>
+                    </li>
                     @foreach($image->commLinks as $commLink)
                         <li class="list-group-item">
                             <a href="{{ route('web.user.rsi.comm-links.show', $commLink->getRouteKey()) }}" class="card-link">{{ $commLink->cig_id }} &mdash; {{ $commLink->title }}</a>
@@ -24,7 +27,7 @@
                 </ul>
                 <div class="card-footer">
                     <a data-toggle="collapse" href="#comm_link_container_{{ $loop->index }}" role="button" aria-expanded="false" aria-controls="comm_link_container_{{ $loop->index }}">
-                        @lang('Comm Links ausklappen')
+                        @lang('Comm Links anzeigen')
                     </a>
                 </div>
             </div>
