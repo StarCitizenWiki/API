0b183e1ab57260f2c2e90670afb9cd6ea8da570d
FoXFTW <hannes@octofox.de>
2018-10-09 17:29:49 +0200

    :fire: Remove Test Api Code

--- app/Http/Controllers/Web/User/DashboardController.php
+++ app/Http/Controllers/Web/User/DashboardController.php
@@ -5,13 +5,7 @@ namespace App\Http\Controllers\Web\User;
 use App\Http\Controllers\Controller;
 use App\Models\Account\User\User;
 use Carbon\Carbon;
-use GuzzleHttp\Client;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Session;
-use MediaWiki\OAuthClient\Consumer;
-use MediaWiki\OAuthClient\Request;
-use MediaWiki\OAuthClient\SignatureMethod\HmacSha1;
-use MediaWiki\OAuthClient\Token;
 
 /**
  * Class DashboardController
@@ -40,90 +34,6 @@ class DashboardController extends Controller
         app('Log')::debug(make_name_readable(__FUNCTION__));
 
         $today = Carbon::today()->toDateString();
-
-        $params = [
-            'action' => 'query',
-            'prop' => 'info',
-            'meta' => 'tokens',
-            'titles' => 'Test',
-            'format' => 'json',
-        ];
-
-
-        $consumer = new Consumer(config('services.mediawiki.client_id'), config('services.mediawiki.client_secret'));
-
-        $userToken = Session::get('oauth.user_token');
-        $userSecret = Session::get('oauth.user_secret');
-        $accessToken = new Token($userToken, $userSecret);
-        $request = Request::fromConsumerAndToken(
-            $consumer,
-            $accessToken,
-            'GET',
-            config('api.wiki_url').'/api.php',
-            $params
-        );
-        $request->signRequest(new HmacSha1(), $consumer, $accessToken);
-        $header = $request->toHeader();
-
-        $header = explode(':', $header);
-
-        $client = new Client(
-            [
-                'headers' => [
-                    $header[0] => $header[1],
-                ],
-            ]
-        );
-        $response = $client->get(
-            config('api.wiki_url').'/api.php?'.http_build_query($params)
-        );
-
-        $token = json_decode($response->getBody()->getContents(), true)['query']['tokens']['csrftoken'];
-
-
-
-
-
-
-
-
-        $apiParams = [
-            'action' => 'edit',
-            'title' => 'Test',
-            'summary' => 'TestEdit',
-            'text' => 'ABC Test 123',
-            'token' => $token,
-        ];
-
-        $request = Request::fromConsumerAndToken(
-            $consumer,
-            $accessToken,
-            'POST',
-            config('api.wiki_url').'/api.php',
-            $apiParams
-        );
-        $request->signRequest(new HmacSha1(), $consumer, $accessToken);
-        $header = $request->toHeader();
-
-        $header = explode(':', $header);
-
-        $client = new Client(
-            [
-                'headers' => [
-                    $header[0] => $header[1],
-                ],
-            ]
-        );
-        $response = $client->post(
-            config('api.wiki_url').'/api.php',
-            [
-                'form_params' => $apiParams,
-            ]
-        );
-
-        dump($response->getStatusCode());
-        dump($response->getHeaders());
-        dd(json_decode($response->getBody()->getContents()));
 
         $users = [
             'overall' => User::all()->count(),
