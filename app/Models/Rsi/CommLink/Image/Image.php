bd78e2139695add3fc5186948e7f721674396a84
Hannes <hannes@octofox.de>
2018-10-11 13:29:35 +0200

    :bug: Fix layoutscache Images not downloading

--- app/Models/Rsi/CommLink/Image/Image.php
+++ app/Models/Rsi/CommLink/Image/Image.php
@@ -50,7 +50,7 @@ class Image extends Model
     {
         $url = config('api.rsi_url');
 
-        if (!starts_with($this->src, '/media') && !starts_with($this->src, '/rsi')) {
+        if (!starts_with($this->src, ['/media', '/rsi', '/layoutscache'])) {
             $url = 'https://media.robertsspaceindustries.com';
         }
 
