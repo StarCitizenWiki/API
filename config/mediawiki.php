c03819a2dbd29d2d32342a56a1e4d905b0df39cc
Hannes <hannes@octofox.de>
2018-10-17 20:55:29 +0200

    :sparkles: Add mediawiki.php config

---
+++ config/mediawiki.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+return [
+    'api_url' => env('WIKI_API_URL'),
+
+    'token_driver' => 'session',
+
+    'driver' => [
+        // Session Keys
+        'session' => [
+            'token' => 'oauth.user_token',
+            'secret' => 'oauth.user_secret',
+        ],
+        // Database Fields
+        'database' => [
+            'token' => 'oauth_token',
+            'secret' => 'oauth_secret',
+        ],
+    ],
+];
