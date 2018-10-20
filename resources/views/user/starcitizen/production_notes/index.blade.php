031dfb659e9dbe3d25a2ab62d270d72afd145ddd
Hannes <hannes@octofox.de>
2018-10-20 19:56:01 +0200

    :lipstick: Add Messages and Errors to Vehicle Elements

--- resources/views/user/starcitizen/production_notes/index.blade.php
+++ resources/views/user/starcitizen/production_notes/index.blade.php
@@ -6,6 +6,8 @@
     <div class="card">
         <h4 class="card-header">@lang('Produktionsnotizen')</h4>
         <div class="card-body px-0 table-responsive">
+            @include('components.errors')
+            @include('components.messages')
             @include('user.components.translation_table')
         </div>
     </div>
