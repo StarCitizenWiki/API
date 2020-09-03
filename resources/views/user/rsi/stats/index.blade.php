8ff52836b46cfe01b56ff3337421569a1f51600b
Hannes <hannes@octofox.de>
2020-09-03 14:58:09 +0200

    :sparkles: Add filter to stats view

--- resources/views/user/rsi/stats/index.blade.php
+++ resources/views/user/rsi/stats/index.blade.php
@@ -4,20 +4,43 @@
 
 @section('content')
     <div class="card">
-        <h4 class="card-header">@lang('Spendenstatistiken')</h4>
-        <div class="card-body px-0">
+        <h4 class="card-header">@lang('Statistiken')</h4>
+        <div class="card-body text-center">
+            <div>
+                <h6>Filter alle Daten:</h6>
+                <a class="btn @if($active === 100) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?skip=100">Jeder 100te Datensatz</a>
+                <a class="btn @if($active === 50) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?skip=50">Jeder 50te Datensatz</a>
+                <a class="btn @if($active === 25) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?skip=25">Jeder 25te Datensatz</a>
+                <a class="btn @if($active === 10) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?skip=10">Jeder 10te Datensatz</a>
+                <a class="btn @if($active === 5) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?skip=5">Jeder 5 Datensatz</a>
+                <a class="btn @if($active === 0) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?skip=0">Alle Daten</a>
+            </div>
+            <div class="mt-3">
+                <h6>Filter Zeit:</h6>
+                <a class="btn @if($from === \Carbon\Carbon::now()->subWeek()->format('Y-m-d')) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?from={{\Carbon\Carbon::now()->subWeek()->format('Y-m-d')}}">Letzte Woche</a>
+                <a class="btn @if($from === \Carbon\Carbon::now()->subMonths(1)->format('Y-m-d')) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?from={{\Carbon\Carbon::now()->subMonths(1)->format('Y-m-d')}}">Letzter Monat</a>
+                <a class="btn @if($from === \Carbon\Carbon::now()->subMonths(3)->format('Y-m-d')) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?from={{\Carbon\Carbon::now()->subMonths(3)->format('Y-m-d')}}">Letzte 3 Monate</a>
+                <a class="btn @if($from === \Carbon\Carbon::now()->subMonths(6)->format('Y-m-d')) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?from={{\Carbon\Carbon::now()->subMonths(6)->format('Y-m-d')}}">Letzte 6 Monate</a>
+                <a class="btn @if($from === \Carbon\Carbon::now()->subYear()->format('Y-m-d')) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?from={{\Carbon\Carbon::now()->subYear()->format('Y-m-d')}}">Letztes Jahr</a>
+                <a class="btn @if($from === \Carbon\Carbon::now()->subYears(2)->format('Y-m-d')) btn-dark @else btn-outline-dark @endif" href="{{ route('web.user.rsi.stat.index') }}?from={{\Carbon\Carbon::now()->subYears(2)->format('Y-m-d')}}">Letzte 2 Jahre</a>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <h4 class="card-header">@lang('Spenden')</h4>
+        <div class="card-body">
             <canvas id="fundsChart" width="400" height="100"></canvas>
         </div>
     </div>
     <div class="card">
-        <h4 class="card-header">@lang('Fansstatistiken')</h4>
-        <div class="card-body px-0">
+        <h4 class="card-header">@lang('Fans')</h4>
+        <div class="card-body">
             <canvas id="fansChart" width="400" height="100"></canvas>
         </div>
     </div>
     <div class="card">
-        <h4 class="card-header">@lang('Fleetstatistiken')</h4>
-        <div class="card-body px-0">
+        <h4 class="card-header">@lang('Fleet')</h4>
+        <div class="card-body">
             <canvas id="fleetChart" width="400" height="100"></canvas>
             <p class="text-center mt-3">
                 Seit dem 20.11.2019 entspricht Fleet = Fans.
