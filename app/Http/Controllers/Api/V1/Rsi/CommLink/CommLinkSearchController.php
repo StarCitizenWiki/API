796fba1963c41ee8501dd4155d23b8273a964641
Hannes <hannes@octofox.de>
2020-09-20 14:24:55 +0200

    :recycle: Split CommLinkSearchController flow into multiple methods

--- app/Http/Controllers/Api/V1/Rsi/CommLink/CommLinkSearchController.php
+++ app/Http/Controllers/Api/V1/Rsi/CommLink/CommLinkSearchController.php
@@ -10,16 +10,19 @@ use App\ImageHash\Implementations\PerceptualHash2;
 use App\Jobs\Rsi\CommLink\Parser\Element\Image as ImageParser;
 use App\Models\Rsi\CommLink\CommLink;
 use App\Models\Rsi\CommLink\Image\Image;
+use App\Models\Rsi\CommLink\Image\ImageHash as ImageHashModel;
 use App\Transformers\Api\V1\Rsi\CommLink\CommLinkTransformer;
 use App\Transformers\Api\V1\Rsi\CommLink\Image\ImageHashTransformer;
 use Dingo\Api\Http\Request;
 use Dingo\Api\Http\Response;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\DB;
 use Jenssegers\ImageHash\ImageHash;
 use Jenssegers\ImageHash\Implementations\AverageHash;
 use Jenssegers\ImageHash\Implementations\DifferenceHash;
-use League\Fractal\TransformerAbstract;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * Class CommLinkController
@@ -27,16 +30,9 @@ use League\Fractal\TransformerAbstract;
 class CommLinkSearchController extends ApiController
 {
     /**
-     * Comm-Link Transformer
-     *
-     * @var CommLinkTransformer
-     */
-    protected TransformerAbstract $transformer;
-
-    /**
      * CommLinkController constructor.
      *
-     * @param Request $request
+     * @param Request             $request
      * @param CommLinkTransformer $transformer
      */
     public function __construct(Request $request, CommLinkTransformer $transformer)
@@ -56,16 +52,12 @@ class CommLinkSearchController extends ApiController
     {
         $request->validate((new CommLinkSearchRequest())->rules());
 
-        $keyword = $request->get('keyword');
-
-        $commLinks = CommLink::query()
-            ->where('title', 'LIKE', sprintf('%%%s%%', $keyword))
-            ->limit(100);
-
-        // Disable pagination
-        $this->limit = 0;
-
-        return $this->getResponse($commLinks);
+        return $this->disablePagination()
+            ->getResponse(
+                CommLink::query()
+                    ->where('title', 'LIKE', sprintf('%%%s%%', $request->get('keyword')))
+                    ->limit(100)
+            );
     }
 
     /**
@@ -80,29 +72,40 @@ class CommLinkSearchController extends ApiController
     {
         $request->validate((new ReverseImageLinkSearchRequest())->rules());
 
-        $url = $request->get('url', '');
-        $url = ImageParser::cleanImgSource($url);
+        /** @var Image $image */
+        $image = Image::query()
+            ->where(
+                'dir',
+                $this->getDirHashFromImageUrl($request->get('url', ''))
+            )
+            ->firstOr(
+                ['*'],
+                function () {
+                    return [];
+                }
+            );
 
-        $url = parse_url($url, PHP_URL_PATH);
-        $dir = ImageParser::getDirHash($url);
+        $this->transformer->includeAllAvailableIncludes();
 
-        if ($dir === false) {
-            return $this->getResponse(collect([]));
-        }
+        return $this->disablePagination()
+            ->getResponse(optional($image)->commLinks);
+    }
 
-        try {
-            /** @var Image $image */
-            $image = Image::query()->where('dir', $dir)->firstOrFail();
-        } catch (ModelNotFoundException $e) {
-            return $this->getResponse(collect([]));
-        }
-
-        $this->transformer->setDefaultIncludes($this->transformer->getAvailableIncludes());
-
-        // Disable pagination
-        $this->limit = 0;
-
-        return $this->getResponse(optional($image)->commLinks);
+    /**
+     * Returns the RSI directory hash of an image url
+     *
+     * @param string $url The RSI Media URl
+     *
+     * @return string The directory hash of the image
+     */
+    private function getDirHashFromImageUrl(string $url): string
+    {
+        return ImageParser::getDirHash(
+            parse_url(
+                ImageParser::cleanImgSource($url),
+                PHP_URL_PATH
+            )
+        );
     }
 
     /**
@@ -114,83 +117,160 @@ class CommLinkSearchController extends ApiController
      */
     public function reverseImageSearch(Request $request): Response
     {
+        $this->checkExtensionsLoaded();
+
+        $request->validate((new ReverseImageSearchRequest())->rules());
+
+        $this->transformer = new ImageHashTransformer();
+        $this->transformer->includeAllAvailableIncludes();
+
+        $hashConfig = $this->getHashConfigForMethod($request->get('method'));
+        $hashConfig['similarity'] = $request->get('similarity');
+        $hashData = $this->hashImage($hashConfig['hasher'], $request->file('image'));
+
+        return $this->disablePagination()
+            ->getResponse(
+                $this->getHashesFromDatabase($hashConfig, $hashData)
+                    ->map(
+                        function (object $data) {
+                            $id = $data->comm_link_image_id;
+                            $image = Image::query()->find($id);
+                            $image->similarity = round((1 - $data->distance / 64) * 100);
+
+                            return $image;
+                        }
+                    )->sortByDesc('similarity')
+            );
+    }
+
+    /**
+     * Checks if either GD or Imagick is loaded
+     *
+     * @throws HttpException
+     */
+    private function checkExtensionsLoaded(): void
+    {
         if (!extension_loaded('gd') && !extension_loaded('imagick')) {
             app('Log')::error('Required extension "GD" or "Imagick" not available.');
 
             $this->response->error('Required extension "GD" or "Imagick" not available.', 501);
         }
+    }
 
-        $request->validate((new ReverseImageSearchRequest())->rules());
-
-        $this->transformer = new ImageHashTransformer();
-        $this->transformer->setDefaultIncludes($this->transformer->getAvailableIncludes());
-
-        $distance = $request->get('similarity', 10);
-        if (!is_numeric($distance) || $distance < 0 || $distance > 100) {
-            $distance = 10;
-        }
-
-        $method = $request->get('method');
-        switch ($method) {
+    /**
+     * Hash config based on hash method
+     *
+     * @param string $hashMethod
+     *
+     * @return array
+     */
+    private function getHashConfigForMethod(string $hashMethod): array
+    {
+        switch ($hashMethod) {
             case 'average':
-                $hasher = new ImageHash(new AverageHash());
-                $prefix = 'a';
-                $table = 'average_hash';
-                break;
+                return [
+                    'hasher' => new ImageHash(new AverageHash()),
+                    'prefix' => 'a',
+                    'table' => 'average_hash',
+                ];
 
             case 'difference':
-                $hasher = new ImageHash(new DifferenceHash());
-                $prefix = 'd';
-                $table = 'difference_hash';
-                break;
+                return [
+                    'hasher' => new ImageHash(new DifferenceHash()),
+                    'prefix' => 'd',
+                    'table' => 'difference_hash',
+                ];
 
             case 'perceptual':
             default:
-                $hasher = new ImageHash(new PerceptualHash2());
-                $prefix = 'p';
-                $table = 'perceptual_hash';
-                break;
+                return [
+                    'hasher' => new ImageHash(new PerceptualHash2()),
+                    'prefix' => 'p',
+                    'table' => 'perceptual_hash',
+                ];
         }
+    }
 
-        $hex = $hasher->hash($request->file('image'))->toHex();
-        $hash = $hex;
-        $hex = str_split($hex, strlen($hex) / 2);
-        $hex = array_map('hexdec', $hex);
+    /**
+     * Hashes an uploaded image
+     *
+     * @param ImageHash    $hasher The hasher with set hash method
+     * @param UploadedFile $file   The uploaded file
+     *
+     * @return array
+     */
+    private function hashImage(ImageHash $hasher, $file): array
+    {
+        $hash = $hasher->hash($file)->toHex();
 
+        return [
+            'hash' => $hash,
+            'decoded' => array_map('hexdec', (str_split($hash, strlen($hash) / 2))),
+        ];
+    }
+
+    /**
+     * Return hashes based on database connection type
+     *
+     * @param array $hashConfig
+     * @param array $hashData
+     *
+     * @return Builder[]|Collection|\Illuminate\Support\Collection
+     */
+    private function getHashesFromDatabase(array $hashConfig, array $hashData)
+    {
         // Since SQLITE does not support the BIT_COUNT operation we only search for exact hash matches
         if (config('database.default') === 'sqlite') {
-            $hashes = \App\Models\Rsi\CommLink\Image\ImageHash::query()->where($table, $hash)->get(
-                'comm_link_image_id'
-            );
-        } else {
-            $hashes = DB::table('comm_link_image_hashes')
-                ->select('comm_link_image_id')
-                ->selectRaw(
-                    'BIT_COUNT('.$prefix.'_hash_1 ^ ?) + BIT_COUNT('.$prefix.'_hash_2 ^ ?) AS distance',
-                    [$hex[0], $hex[1]]
-                )
-                ->havingRaw('distance <= ?', [$distance])
-                ->limit(50)
-                ->get();
+            return $this->getHashesFromSQLiteStore($hashConfig['table'], $hashData['hash']);
         }
 
-        if ($hashes->isEmpty()) {
-            return $this->getResponse(collect([]));
-        }
+        return $this->getHashesFromSQLStore(
+            $hashConfig['prefix'],
+            $hashData['decoded'],
+            $hashConfig['similarity']
+        );
+    }
 
-        $images = $hashes->map(
-            function (object $data) {
-                $id = $data->comm_link_image_id;
-                $image = Image::query()->find($id);
-                $image->similarity = round((1 - $data->distance / 64) * 100);
+    /**
+     * Get the image hashes that equal the provided hash
+     *
+     * @param string $hashMethod Hash method average, distance, perceptual
+     * @param string $hash       The image hash
+     *
+     * @return Builder[]|Collection
+     */
+    private function getHashesFromSQLiteStore(string $hashMethod, string $hash)
+    {
+        return ImageHashModel::query()
+            ->where($hashMethod, $hash)
+            ->get('comm_link_image_id');
+    }
 
-                return $image;
-            }
-        )->sortByDesc('similarity');
-
-        // Disable pagination
-        $this->limit = 0;
-
-        return $this->getResponse($images);
+    /**
+     * Get the image hashes matching the provided hash method and hamming distance
+     *
+     * @param string $prefix      Hash Attribute prefix
+     * @param array  $decodedHash Image hash split in the middle and hex decoded
+     * @param int    $distance    The maximum hamming distance
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    private function getHashesFromSQLStore(
+        string $prefix,
+        array $decodedHash,
+        int $distance
+    ): \Illuminate\Support\Collection {
+        return DB::table('comm_link_image_hashes')
+            ->select('comm_link_image_id')
+            ->selectRaw(
+                'BIT_COUNT('.$prefix.'_hash_1 ^ ?) + BIT_COUNT('.$prefix.'_hash_2 ^ ?) AS distance',
+                [
+                    $decodedHash[0],
+                    $decodedHash[1],
+                ]
+            )
+            ->havingRaw('distance <= ?', [$distance])
+            ->limit(50)
+            ->get();
     }
 }
