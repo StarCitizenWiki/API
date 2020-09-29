607a76f853282127b04397908183c21d6462e108
Hannes <hannes@octofox.de>
2020-09-29 22:48:56 +0200

    :sparkles: Add celestial object includes to jumppoint transformer

--- app/Transformers/Api/V1/StarCitizen/Starmap/JumppointTransformer.php
+++ app/Transformers/Api/V1/StarCitizen/Starmap/JumppointTransformer.php
@@ -6,12 +6,19 @@ namespace App\Transformers\Api\V1\StarCitizen\Starmap;
 
 use App\Models\Api\StarCitizen\Starmap\Jumppoint\Jumppoint;
 use App\Transformers\Api\V1\StarCitizen\AbstractTranslationTransformer;
+use League\Fractal\Resource\Item;
+use Throwable;
 
 /**
  * Class JumppointTransformer
  */
 class JumppointTransformer extends AbstractTranslationTransformer
 {
+    protected $availableIncludes = [
+        'celestial_object_entry',
+        'celestial_object_exit',
+    ];
+
     /**
      * @param Jumppoint $jumppoint
      *
@@ -54,5 +61,31 @@ class JumppointTransformer extends AbstractTranslationTransformer
                 'designation' => $jumppoint->exit->designation,
             ],
         ];
+    }
+
+    /**
+     * The celestial object of the jump point entry
+     *
+     * @param Jumppoint $jumppoint
+     *
+     * @return Item
+     * @throws Throwable
+     */
+    public function includeCelestialObjectEntry(Jumppoint $jumppoint): Item
+    {
+        return $this->item($jumppoint->entry, $this->makeTransformer(CelestialObjectTransformer::class, $this));
+    }
+
+    /**
+     * The celestial object of the jump point exit
+     *
+     * @param Jumppoint $jumppoint
+     *
+     * @return Item
+     * @throws Throwable
+     */
+    public function includeCelestialObjectExit(Jumppoint $jumppoint): Item
+    {
+        return $this->item($jumppoint->exit, $this->makeTransformer(CelestialObjectTransformer::class, $this));
     }
 }
