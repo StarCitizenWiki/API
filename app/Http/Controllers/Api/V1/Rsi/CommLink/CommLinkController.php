72e3564894c4dd326affadc7e9c626ebd0ccc98b
Hannes <hannes@octofox.de>
2020-12-07 20:51:55 +0100

    :memo: WIP API Docs

--- app/Http/Controllers/Api/V1/Rsi/CommLink/CommLinkController.php
+++ app/Http/Controllers/Api/V1/Rsi/CommLink/CommLinkController.php
@@ -12,7 +12,10 @@ use Dingo\Api\Http\Response;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
- * Class CommLinkController
+ * Comm-Link API
+ * Scraped Comm-Links from Roberts Space Industries
+ *
+ * @Resource("Comm-Links", uri="/comm-links")
  */
 class CommLinkController extends ApiController
 {
@@ -29,7 +32,77 @@ class CommLinkController extends ApiController
     }
 
     /**
-     * Ausgabe aller Comm-Links
+     * Returns all Comm-Links
+     *
+     * @Get("/{?page,limit,include}")
+     * @Versions({"v1"})
+     * @Parameters({
+     *     @Parameter("page", type="integer", required=false, description="Pagination page", default=1),
+     *     @Parameter(
+     *          "include",
+     *          type="string",
+     *          required=false,
+     *          description="Relations to include. Valid relations are shown in the meta data"
+     *     ),
+     *     @Parameter(
+     *          "limit",
+     *          type="integer",
+     *          required=false,
+     *          description="Items per page, set to 0, to return all items",
+     *          default=10
+     *     ),
+     * })
+     * @Request(headers={"Accept": "application/x.StarCitizenWikiApi.v1+json"})
+     * @Response(200, body={
+     * "data": {
+     *  {
+     *      "id": 17911,
+     *      "title": "Star Citizen Live",
+     *      "rsi_url": "https:\/\/robertsspaceindustries.com\/comm-link\/transmission\/17911-Star-Citizen-Live",
+     *      "api_url": "https:\/\/api.star-citizen.wiki\/api\/comm-links\/17911",
+     *      "api_public_url": "https:\/\/api.star-citizen.wiki\/comm-links\/17911",
+     *      "channel": "Transmission",
+     *      "category": "General",
+     *      "series": "Star Citizen LIVE",
+     *      "images": 1,
+     *      "links": 2,
+     *      "comment_count": 4,
+     *      "created_at": "2020-12-03T23:00:00.000000Z"
+     *  },
+     *  {
+     *      "id": 17909,
+     *      "title": "Inside Star Citizen",
+     *      "rsi_url": "https:\/\/robertsspaceindustries.com\/comm-link\/transmission\/17909-Inside-Star-Citizen",
+     *      "api_url": "https:\/\/api.star-citizen.wiki\/api\/comm-links\/17909",
+     *      "api_public_url": "https:\/\/api.star-citizen.wiki\/comm-links\/17909",
+     *      "channel": "Transmission",
+     *      "category": "General",
+     *      "series": "Inside Star Citizen",
+     *      "images": 1,
+     *      "links": 2,
+     *      "comment_count": 18,
+     *      "created_at": "2020-12-02T23:00:00.000000Z"
+     *  },
+     * },
+     * "meta": {
+     *      "processed_at": "2020-12-07 14:45:18",
+     *      "valid_relations": {
+     *          "images",
+     *          "links",
+     *          "english",
+     *          "german"
+     *      },
+     *      "pagination": {
+     *          "total": 4229,
+     *          "count": 15,
+     *          "per_page": 15,
+     *          "current_page": 1,
+     *          "total_pages": 282,
+     *          "links": {
+     *          "next": "https:\/\/api.star-citizen.wiki\/api\/comm-links?page=2"
+     *      }
+     * }
+     * })
      *
      * @return Response
      */
@@ -42,6 +115,68 @@ class CommLinkController extends ApiController
 
     /**
      * Returns a singular comm-link by its cig_id
+     *
+     * @Get("/{ID}{?include}")
+     * @Versions({"v1"})
+     * @Parameters({
+     *     @Parameter("ID", type="interger", required=true, description="Comm-Link ID"),
+     *     @Parameter(
+     *          "include",
+     *          type="string",
+     *          required=false,
+     *          description="Relations to include. Valid relations are shown in the meta data"
+     *     ),
+     * })
+     * @Request(headers={"Accept": "application/x.StarCitizenWikiApi.v1+json"})
+     * @Response(200, body={
+     * "data": {
+     *      "id": 17911,
+     *      "title": "Star Citizen Live",
+     *      "rsi_url": "https:\/\/robertsspaceindustries.com\/comm-link\/transmission\/17911-Star-Citizen-Live",
+     *      "api_url": "https:\/\/api.star-citizen.wiki\/api\/comm-links\/17911",
+     *      "api_public_url": "https:\/\/api.star-citizen.wiki\/comm-links\/17911",
+     *      "channel": "Transmission",
+     *      "category": "General",
+     *      "series": "Star Citizen LIVE",
+     *      "images": {
+     *          "data": {
+     *              {
+     *                  "rsi_url": "...",
+     *                  "api_url": null,
+     *                  "alt": "",
+     *                  "size": 18693,
+     *                  "mime_type": "image\/png",
+     *                  "last_modified": "2016-05-05T01:15:45.000000Z"
+     *              }
+     *          }
+     *      },
+     *      "links": {
+     *          "data": {
+     *              {
+     *                  "href": "http:\/\/twitch.tv\/starcitizen",
+     *                  "text": "http:\/\/twitch.tv\/starcitizen"
+     *              },
+     *              {
+     *                  "href": "https:\/\/www.youtube.com\/embed\/gsWDdomcMCM?wmode=transparent",
+     *                  "text": "iframe"
+     *              }
+     *          }
+     *      },
+     *      "comment_count": 4,
+     *      "created_at": "2020-12-03T23:00:00.000000Z"
+     * },
+     * "meta": {
+     *      "processed_at": "2020-12-07 14:52:11",
+     *      "valid_relations": {
+     *          "images",
+     *          "links",
+     *          "english",
+     *          "german"
+     *      },
+     *      "prev_id": 17909,
+     *      "next_id": -1
+     * }
+     * })
      *
      * @param int $commLink
      *
--- app/Http/Controllers/Api/V1/StarCitizen/Manufacturer/ManufacturerController.php
+++ app/Http/Controllers/Api/V1/StarCitizen/Manufacturer/ManufacturerController.php
@@ -13,7 +13,10 @@ use Dingo\Api\Http\Response;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
- * Hersteller API
+ * Manufacturer API
+ * Manufacturers found in the ShipMatrix
+ *
+ * @Resource("Manufacturers", uri="/manufacturers")
  */
 class ManufacturerController extends ApiController
 {
@@ -30,7 +33,72 @@ class ManufacturerController extends ApiController
     }
 
     /**
-     * Alle Hersteller
+     * Returns all manufacturers
+     *
+     * @Get("/{?page,limit,include}")
+     * @Versions({"v1"})
+     * @Parameters({
+     *     @Parameter("page", type="integer", required=false, description="Pagination page", default=1),
+     *     @Parameter(
+     *          "include",
+     *          type="string",
+     *          required=false,
+     *          description="Relations to include. Valid relations are shown in the meta data"
+     *     ),
+     *     @Parameter(
+     *          "limit",
+     *          type="integer",
+     *          required=false,
+     *          description="Items per page, set to 0, to return all items",
+     *          default=10
+     *     ),
+     * })
+     * @Request(headers={"Accept": "application/x.StarCitizenWikiApi.v1+json"})
+     * @Response(200, body={
+     * "data": {
+     *  {
+     *      "code": "RSI",
+     *      "name": "Roberts Space Industries",
+     *      "known_for": {
+     *          "de_DE": "Die Aurora und die Constellation",
+     *          "en_EN": "the Aurora and the Constellation"
+     *      },
+     *      "description": {
+     *          "de_DE": "...",
+     *          "en_EN": "..."
+     *      },
+     *  },
+     *  {
+     *      "code": "ORIG",
+     *      "name": "Origin Jumpworks GmbH",
+     *      "known_for": {
+     *          "de_DE": "Die 300i Serie",
+     *          "en_EN": "the 300i series"
+     *      },
+     *      "description": {
+     *          "de_DE": "...",
+     *          "en_EN": "..."
+     *      },
+     *  },
+     * },
+     * "meta": {
+     *  "processed_at": "2020-12-07 13:25:54",
+     *  "valid_relations": {
+     *      "ships",
+     *      "vehicles"
+     *  },
+     *  "pagination": {
+     *      "total": 17,
+     *      "count": 10,
+     *      "per_page": 10,
+     *      "current_page": 1,
+     *      "total_pages": 2,
+     *      "links": {
+     *          "next": "https:\/\/api.star-citizen.wiki\/api\/manufacturers?page=2"
+     *      }
+     *  }
+     * }
+     * })
      *
      * @return Response
      */
@@ -40,7 +108,43 @@ class ManufacturerController extends ApiController
     }
 
     /**
-     * Einzelner Hersteller
+     * Returns a single manufacturer
+     *
+     * @Get("/{CODE}{?include}")
+     * @Versions({"v1"})
+     * @Parameters({
+     *     @Parameter("CODE", type="string", required=true, description="Manufacturer Code"),
+     *     @Parameter(
+     *          "include",
+     *          type="string",
+     *          required=false,
+     *          description="Relations to include. Valid relations are shown in the meta data"
+     *     ),
+     * })
+     * @Request(headers={"Accept": "application/x.StarCitizenWikiApi.v1+json"})
+     * @Response(200, body={
+     * "data": {
+     *  {
+     *      "code": "RSI",
+     *      "name": "Roberts Space Industries",
+     *      "known_for": {
+     *          "de_DE": "Die Aurora und die Constellation",
+     *          "en_EN": "the Aurora and the Constellation"
+     *      },
+     *      "description": {
+     *          "de_DE": "...",
+     *          "en_EN": "..."
+     *      },
+     *  }
+     * },
+     * "meta": {
+     *  "processed_at": "2020-12-07 13:25:54",
+     *  "valid_relations": {
+     *      "ships",
+     *      "vehicles"
+     *  },
+     * }
+     * })
      *
      * @param string $manufacturer
      *
@@ -51,7 +155,7 @@ class ManufacturerController extends ApiController
         $manufacturer = urldecode($manufacturer);
 
         try {
-            $manufacturer = Manufacturer::query()
+            $model = Manufacturer::query()
                 ->where('name_short', $manufacturer)
                 ->orWhere('name', $manufacturer)
                 ->firstOrFail();
@@ -59,11 +163,54 @@ class ManufacturerController extends ApiController
             $this->response->errorNotFound(sprintf(static::NOT_FOUND_STRING, $manufacturer));
         }
 
-        return $this->getResponse($manufacturer);
+        return $this->getResponse($model);
     }
 
     /**
      * Search Endpoint
+     *
+     * @Post("/search")
+     * @Versions({"v1"})
+     * @Parameters({
+     *     @Parameter("query", type="string", required=true, description="Manufacturer Code or partial name"),
+     *     @Parameter(
+     *          "include",
+     *          type="string",
+     *          required=false,
+     *          description="Relations to include. Valid relations are shown in the meta data"
+     *     ),
+     * })
+     *
+     * @Transaction({
+     *      @Request({"query": "RSI"}, headers={"Accept": "application/x.StarCitizenWikiApi.v1+json"}),
+     *      @Response(200, body={
+     *      "data": {
+     *       {
+     *           "code": "RSI",
+     *           "name": "Roberts Space Industries",
+     *           "known_for": {
+     *               "de_DE": "Die Aurora und die Constellation",
+     *               "en_EN": "the Aurora and the Constellation"
+     *           },
+     *           "description": {
+     *               "de_DE": "...",
+     *               "en_EN": "..."
+     *           },
+     *       }
+     *      },
+     *      "meta": {
+     *       "processed_at": "2020-12-07 13:25:54",
+     *       "valid_relations": {
+     *           "ships",
+     *           "vehicles"
+     *       },
+     *      }
+     *      }),
+     *
+     *      @Request({"query": "INVALID"}, headers={"Accept": "application/x.StarCitizenWikiApi.v1+json"}),
+     *      @Response(404, body={"message": "No Results for Query 'INVALID'", "status_code": 404})
+     * })
+     *
      *
      * @param Request $request
      *
