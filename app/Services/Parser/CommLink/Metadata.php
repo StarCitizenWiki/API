83b2fec57eafbda812e546696b13764fc95adde5
Hannes <hannes@octofox.de>
2021-11-19 10:01:51 +0100

    :recycle: Manually set some metadata
    As the new layout system hides the top bar containing that data, we try to manually set this based on the Comm-Link title

--- app/Services/Parser/CommLink/Metadata.php
+++ app/Services/Parser/CommLink/Metadata.php
@@ -11,6 +11,7 @@ use App\Models\Rsi\CommLink\Series\Series;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
+use InvalidArgumentException;
 use Symfony\Component\DomCrawler\Crawler;
 
 /**
@@ -54,6 +55,71 @@ class Metadata extends BaseElement
     private const SUBSCRIBER = 'Subscriber';
 
     /**
+     * This maps a common Comm-Link Title to pre-defined Channel / Category / Series
+     * As the "new" Layout Systems hides the top-bar that includes this information we need to manually set this...
+     */
+    private const MANUAL_SETTINGS = [
+        '/Inside Star Citizen/' => [
+            'channel' => 'Transmission',
+            'category' => 'General',
+            'series' => 'Inside Star Citizen',
+        ],
+        '/Star Citizen Live(?:\sGamedev)?/' => [
+            'channel' => 'Transmission',
+            'category' => 'General',
+            'series' => 'Star Citizen LIVE',
+        ],
+        // Q&A: ... Posts
+        '/Q\s?&\s?A:?.*/' => [
+            'channel' => 'Engineering',
+            'category' => 'Development',
+            // TODO: Is this correct for all?
+            'series' => 'Concept Ship Q&A',
+        ],
+        // ... Q&A Posts
+        '/.*Q\s?&\s?A$/' => [
+            'channel' => 'Engineering',
+            'category' => 'Development',
+        ],
+        // Roadmap Roundup ... Posts
+        '/Roadmap Roundup.*/' => [
+            'channel' => 'Spectrum Dispatch',
+            'category' => 'Lore',
+            'series' => 'Roadmap Roundup',
+        ],
+        // ... Subscriber Promotions
+        '/.*Subscriber Promotions$/' => [
+            // This is not an official series
+            'series' => 'Subscriber Promotions',
+        ],
+        // Star Citizen Alpha ... Posts
+        '/Calling All Devs/' => [
+            'channel' => 'Transmission',
+            'category' => 'General',
+            'series' => 'Calling All Devs',
+        ],
+        // Star Citizen Alpha ... Posts
+        '/Star Citizen (Alpha|Beta) [\d\.]+\s?(?:Available!?)?$/' => [
+            'channel' => 'Transmission',
+            'category' => 'General',
+            // This is not an official series
+            'series' => 'Release Info',
+        ],
+        // Alpha - ... Posts
+        '/(Alpha|Beta) - .*/' => [
+            'channel' => 'Transmission',
+            'category' => 'General',
+            // This is not an official series
+            'series' => 'Release Info',
+        ],
+    ];
+
+    /**
+     * Title used if no title could be found
+     */
+    private const NO_TITLE_FOUND = 'No Title Found';
+
+    /**
      * @var Crawler
      */
     private Crawler $commLink;
@@ -87,6 +153,8 @@ class Metadata extends BaseElement
         $this->extractCommentCount();
         $this->extractCreatedAt();
 
+        $this->runManualFixes();
+
         return $this->metaData;
     }
 
@@ -103,8 +171,8 @@ class Metadata extends BaseElement
 
         try {
             $title = $this->commLink->filterXPath('//title')->text();
-        } catch (\InvalidArgumentException $e) {
-            $title = 'No Title Found';
+        } catch (InvalidArgumentException $e) {
+            $title = self::NO_TITLE_FOUND;
         }
 
         $title = preg_replace(
@@ -133,8 +201,8 @@ class Metadata extends BaseElement
     {
         try {
             $title = $this->commLink->filter('.title-section h2')->first()->text();
-        } catch (\InvalidArgumentException $e) {
-            $title = 'No Title Found';
+        } catch (InvalidArgumentException $e) {
+            $title = self::NO_TITLE_FOUND;
         }
 
         $this->metaData->put('title', $this->cleanText($title));
@@ -143,24 +211,28 @@ class Metadata extends BaseElement
     /**
      * Tries to Extract the Comm-Link Category
      * Defaults to 'Undefined' if not found
+     *
+     * @param string|null $category Manual category to use
      */
-    private function extractCategory(): void
+    private function extractCategory(?string $category = null): void
     {
         $categoryId = 1;
 
-        if ($this->commLink->filter(self::CATEGORY_SELECTOR)->count() > 0) {
+        if ($category === null && $this->commLink->filter(self::CATEGORY_SELECTOR)->count() > 0) {
             $category = $this->commLink->filter(self::CATEGORY_SELECTOR)->text();
 
             if (!empty($category)) {
                 $category = $this->cleanText($category);
-
-                $categoryId = Category::query()->firstOrCreate(
-                    [
-                        'name' => $category,
-                        'slug' => Str::slug($category, '-'),
-                    ]
-                )->id;
             }
+        }
+
+        if (!empty($category)) {
+            $categoryId = Category::query()->firstOrCreate(
+                [
+                    'name' => $category,
+                    'slug' => Str::slug($category, '-'),
+                ]
+            )->id;
         }
 
         $this->metaData->put('category_id', $categoryId);
@@ -169,28 +241,34 @@ class Metadata extends BaseElement
     /**
      * Tries to Extract the Comm-Link Channel
      * Defaults to 'Undefined' if not found
+     *
+     * @param string|null $channel Manual channel to use
      */
-    private function extractChannel(): void
+    private function extractChannel(?string $channel = null): void
     {
         $channelId = 1;
 
-        if ($this->commLink->filter(self::CHANNEL_SELECTOR)->count() > 0 || $this->isSubscriberPage($this->commLink)) {
+        if (
+            $channel === null &&
+            ($this->commLink->filter(self::CHANNEL_SELECTOR)->count() > 0 ||
+                $this->isSubscriberPage($this->commLink))
+        ) {
             if ($this->isSubscriberPage($this->commLink)) {
                 $channel = self::SUBSCRIBER;
             } else {
                 $channel = $this->commLink->filter(self::CHANNEL_SELECTOR)->text();
             }
+        }
 
-            if (!empty($channel)) {
-                $channel = $this->cleanText($channel);
+        if (!empty($channel)) {
+            $channel = $this->cleanText($channel);
 
-                $channelId = Channel::query()->firstOrCreate(
-                    [
-                        'name' => $channel,
-                        'slug' => Str::slug($channel, '-'),
-                    ]
-                )->id;
-            }
+            $channelId = Channel::query()->firstOrCreate(
+                [
+                    'name' => $channel,
+                    'slug' => Str::slug($channel, '-'),
+                ]
+            )->id;
         }
 
         $this->metaData->put('channel_id', $channelId);
@@ -199,24 +277,26 @@ class Metadata extends BaseElement
     /**
      * Tries to Extract the Comm-Link Series
      * Defaults to 'None' if not found
+     *
+     * @param string|null $series Manual series to use
      */
-    private function extractSeries(): void
+    private function extractSeries(?string $series = null): void
     {
         $seriesId = Series::query()->first()->id;
 
-        if ($this->commLink->filter(self::SERIES_SELECTOR)->count() > 0) {
+        if ($series === null && $this->commLink->filter(self::SERIES_SELECTOR)->count() > 0) {
             $series = $this->commLink->filter(self::SERIES_SELECTOR)->text();
+        }
 
-            if (!empty($series)) {
-                $series = $this->cleanText($series);
+        if (!empty($series)) {
+            $series = $this->cleanText($series);
 
-                $seriesId = Series::query()->firstOrCreate(
-                    [
-                        'name' => $series,
-                        'slug' => Str::slug($series, '-'),
-                    ]
-                )->id;
-            }
+            $seriesId = Series::query()->firstOrCreate(
+                [
+                    'name' => $series,
+                    'slug' => Str::slug($series, '-'),
+                ]
+            )->id;
         }
 
         $this->metaData->put('series_id', $seriesId);
@@ -270,5 +350,32 @@ class Metadata extends BaseElement
         }
 
         $this->metaData->put('created_at', $createdAt);
+    }
+
+    /**
+     * @see Metadata::MANUAL_SETTINGS
+     */
+    private function runManualFixes(): void
+    {
+        $title = $this->metaData->get('title');
+        if ($title === self::NO_TITLE_FOUND) {
+            return;
+        }
+
+        foreach (self::MANUAL_SETTINGS as $matcher => $data) {
+            if (preg_match($matcher, $title) === 1) {
+                if (isset($data['channel'])) {
+                    $this->extractChannel($data['channel']);
+                }
+                if (isset($data['category'])) {
+                    $this->extractCategory($data['category']);
+                }
+                if (isset($data['series'])) {
+                    $this->extractSeries($data['series']);
+                }
+
+                return;
+            }
+        }
     }
 }
