8eae33c2788bc7aba3068d6e8964963c730ae4cf
Hannes <hannes@octofox.de>
2021-12-29 11:28:20 +0100

    :bug: Remove invalid `$` from mail

--- resources/views/emails/rsi/comm_link/new_comm_link_downloaded.blade.php
+++ resources/views/emails/rsi/comm_link/new_comm_link_downloaded.blade.php
@@ -6,8 +6,8 @@
     @foreach($commLinks as $commLink)
         <li style="margin-top: 0.5rem">
             {{ $commLink->commLink->cig_id }}: {{ $commLink->commLink->title }}
-            <ul>
-                <li style="display: inline"><a href="{{ config('api.rsi_url') }}{{ $commLink->$commLink->url }}}">RSI</a></li>
+            <ul style="display: flex">
+                <li style="display: inline"><a href="{{ config('api.rsi_url') }}{{ $commLink->commLink->url }}}">RSI</a></li>
                 <li style="display: inline">&mdash; <a href="{{ config('services.mediawiki.url') }}/Comm-Link:{{ $commLink->commLink->cig_id }}">Wiki</a> &mdash;</li>
                 <li style="display: inline"><a href="{{ route('web.api.comm-links.show', $commLink->commLink->cig_id) }}">API</a></li>
             </ul>
