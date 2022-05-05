e1c119443a5a288deafd8bb8dd3684acde1a504a
Hannes <hannes@octofox.de>
2022-05-05 22:42:30 +0200

    :bug: Fix unique nullable mail

---
+++ database/migrations/update/users/2022_05_05_222303_remove_email_key_from_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveEmailKeyFromUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropUnique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unique('email');
+        });
+    }
+}
