1551bba35760763d51c7b52ee4edc8f5e23441d3
Hannes <hannes@octofox.de>
2023-01-08 14:37:12 +0100

    refactor: Update CL translation import job

--- app/Jobs/Wiki/CommLink/CreateCommLinkWikiTranslationPages.php
+++ app/Jobs/Wiki/CommLink/CreateCommLinkWikiTranslationPages.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace App\Jobs\Wiki\CommLink;
 
+use App\Jobs\Wiki\ApproveRevisions;
 use App\Models\Rsi\CommLink\CommLink;
-use App\Services\TranslateSectionizer;
-use App\Services\WrappedWiki;
 use App\Traits\GetWikiCsrfTokenTrait as GetWikiCsrfToken;
 use App\Traits\Jobs\GetCommLinkWikiPageInfoTrait as GetCommLinkWikiPageInfo;
 use ErrorException;
@@ -56,72 +55,36 @@ class CreateCommLinkWikiTranslationPages implements ShouldQueue
             return;
         }
 
-        $createPage = static function (int $clId, string $locale, int $id, string $content) use ($token) {
-            MediaWikiApi::edit("Translations:Comm-Link:{$clId}/{$id}/{$locale}")
-                ->text($content)
-                ->summary("Importing Comm-Link Translation")
-                ->csrfToken($token)
-                ->markBotEdit()
-                ->createOnly()
-                ->request();
-        };
-
         CommLink::query()
             ->whereHas(
                 'translations',
                 static function (Builder $query) {
                     $query->where('locale_code', config('services.wiki_translations.locale'))
-                    ->whereRaw(
-                        "translation <> ''"
-                    );
+                        ->whereRaw(
+                            "translation <> ''"
+                        );
                 }
             )->chunk(
                 10,
-                function (Collection $commlinks) use ($createPage) {
-                    $commlinks->filter(function (CommLink $commLink) {
-                        return $commLink->german() !== null;
-                    })
-                        ->map(function (CommLink $commLink) {
-                            $commLink->splittedEnglish = TranslateSectionizer::sectionise(
-                                $commLink->english()->translation
-                            );
+                function (Collection $commlinks) use ($token) {
+                    $commlinks->each(function (CommLink $commLink) use ($token) {
+                        MediaWikiApi::edit("Comm-Link:{$commLink->cig_id}/en")
+                            ->text(sprintf(
+                                "<noinclude>{{Comm-Link}}</noinclude>\n%s",
+                                optional($commLink->english())->translation
+                            ))
+                            ->summary("Importing Comm-Link Translation")
+                            ->csrfToken($token)
+                            ->markBotEdit()
+                            ->request();
+                    });
 
-                            if ($commLink->german()->proofread === true) {
-                                $splittedGerman = TranslateSectionizer::sectionise(
-                                    WrappedWiki::getWikiPageText(sprintf('Comm-Link:%d', $commLink->cig_id)) ?? ''
-                                );
 
-                                $fix = $splittedGerman[0] ?? '';
-                                $fix = str_replace('<noinclude>{{Comm-Link}}</noinclude>', '', $fix);
-                                $fix = explode(']]', $fix);
-                                $splittedGerman[0] = trim(array_pop($fix));
-
-                                $commLink->splittedGerman = $splittedGerman;
-                            } else {
-                                $commLink->splittedGerman = TranslateSectionizer::sectionise(
-                                    $commLink->german()->translation
-                                );
-                            }
-
-                            return $commLink;
-                        })
-                        ->filter(function (CommLink $commLink) {
-                            $equal = count($commLink->splittedEnglish) === count($commLink->splittedGerman);
-
-                            if ($equal === false) {
-                                app('Log')::warning("Splits for Comm-Link {$commLink->cig_id} arent equal!");
-                            }
-
-                            return $equal;
-                        })
-                        ->each(function (CommLink $commLink) use ($createPage) {
-                            for ($i = 1; $i < count($commLink->splittedEnglish) + 1; $i++) {
-                                $createPage($commLink->cig_id, 'en', $i, $commLink->splittedEnglish[$i - 1]);
-                            }
-                            for ($i = 1; $i < count($commLink->splittedGerman) + 1; $i++) {
-                                $createPage($commLink->cig_id, 'de', $i, $commLink->splittedGerman[$i - 1]);
-                            }
-                        });
+                    if (config('services.wiki_approve_revs.access_secret', null) !== null) {
+                        dispatch(new ApproveRevisions($commlinks->map(function (CommLink $commLink) {
+                            return "Comm-Link:{$commLink->cig_id}/en";
+                        })->toArray()));
+                    }
                 }
             );
     }
