6e501dde76049b5f5948810489261ff4e63d8d7d
H. C. Kruse <hannes@octofox.de>
2023-06-18 17:13:56 +0200

    refactor(Mail): Remove indentation from mail views

--- resources/views/emails/rsi/comm_link/comm_link_changed.blade.php
+++ resources/views/emails/rsi/comm_link/comm_link_changed.blade.php
@@ -1,42 +1,42 @@
 @component('mail::message')
-    # @lang('Geänderte Comm-Links'):
+# @lang('Geänderte Comm-Links'):
 
-    ## @lang('Comm-Links mit neuem Inhalt'): {{ $withoutContent->count() }}
-    @lang('Dies sind Comm-Links, welche bisher keinen Inhalt hatten.')
-    @component('mail::panel')
-        <ul>
-            @forelse($withoutContent as $item)
-                <li>
-                    {{ $item->commLink->cig_id }}: {{ $item->commLink->title }}
-                    <span style="float: right">
-                        <a href="{{ route('web.api.comm-links.show', $item->commLink->cig_id) }}">@lang('API')</a> -
-                        <a href="{{ config('services.mediawiki.url') }}/Comm-Link:{{ $item->commLink->cig_id }}">@lang('Wiki')</a>
-                    </span>
-                </li>
-                @empty
-                <li>@lang('Keine Comm-Links geändert')</li>
-            @endforelse
-        </ul>
-    @endcomponent
+## @lang('Comm-Links mit neuem Inhalt'): {{ $withoutContent->count() }}
+@lang('Dies sind Comm-Links, welche bisher keinen Inhalt hatten.')
+@component('mail::panel')
+<ul>
+    @forelse($withoutContent as $item)
+        <li>
+            {{ $item->commLink->cig_id }}: {{ $item->commLink->title }}
+            <span style="float: right">
+                <a href="{{ route('web.api.comm-links.show', $item->commLink->cig_id) }}">@lang('API')</a> -
+                <a href="{{ config('services.mediawiki.url') }}/Comm-Link:{{ $item->commLink->cig_id }}">@lang('Wiki')</a>
+            </span>
+        </li>
+        @empty
+        <li>@lang('Keine Comm-Links geändert')</li>
+    @endforelse
+</ul>
+@endcomponent
 
-    <br>
-    <br>
+<br>
+<br>
 
-    ## @lang('Comm-Links mit geändertem Inhalt'): {{ $withContent->count() }}
-    @lang('Dies sind Comm-Links, dessen Inhalt geändert wurden.')
-    @component('mail::panel')
-        <ul>
-            @forelse($withContent as $item)
-                <li>
-                    {{ $item->commLink->cig_id }}: {{ $item->commLink->title }}
-                    <span style="float: right">
-                        <a href="{{ route('web.api.comm-links.show', $item->commLink->cig_id) }}">@lang('API')</a> -
-                        <a href="{{ config('services.mediawiki.url') }}/Comm-Link:{{ $item->commLink->cig_id }}">@lang('Wiki')</a>
-                    </span>
-                </li>
-                @empty
-                <li>@lang('Keine Comm-Links geändert')</li>
-            @endforelse
-        </ul>
-    @endcomponent
+## @lang('Comm-Links mit geändertem Inhalt'): {{ $withContent->count() }}
+@lang('Dies sind Comm-Links, dessen Inhalt geändert wurden.')
+@component('mail::panel')
+<ul>
+    @forelse($withContent as $item)
+        <li>
+            {{ $item->commLink->cig_id }}: {{ $item->commLink->title }}
+            <span style="float: right">
+                <a href="{{ route('web.api.comm-links.show', $item->commLink->cig_id) }}">@lang('API')</a> -
+                <a href="{{ config('services.mediawiki.url') }}/Comm-Link:{{ $item->commLink->cig_id }}">@lang('Wiki')</a>
+            </span>
+        </li>
+        @empty
+        <li>@lang('Keine Comm-Links geändert')</li>
+    @endforelse
+</ul>
+@endcomponent
 @endcomponent
--- resources/views/emails/rsi/comm_link/new_comm_link_downloaded.blade.php
+++ resources/views/emails/rsi/comm_link/new_comm_link_downloaded.blade.php
@@ -1,18 +1,18 @@
 @component('mail::message')
-    # @lang('Neu importierte Comm-Links'):
+# @lang('Neu importierte Comm-Links'):
 
-    @component('mail::panel')
-    <ul>
-        @foreach($commLinks as $commLink)
-            <li style="margin-top: 0.5rem">
-                {{ $commLink->commLink->cig_id }}: {{ $commLink->commLink->title }}
-                <ul style="display: flex">
-                    <li style="display: inline"><a href="{{ config('api.rsi_url') }}{{ $commLink->commLink->url }}">@lang('RSI')</a></li>
-                    <li style="display: inline">- <a href="{{ route('web.api.comm-links.show', $commLink->commLink->cig_id) }}">@lang('API')</a> -</li>
-                    <li style="display: inline"><a href="{{ config('services.mediawiki.url') }}/Comm-Link:{{ $commLink->commLink->cig_id }}">@lang('Wiki')</a></li>
-                </ul>
-            </li>
-        @endforeach
-    </ul>
-    @endcomponent
+@component('mail::panel')
+<ul>
+    @foreach($commLinks as $commLink)
+        <li style="margin-top: 0.5rem">
+            {{ $commLink->commLink->cig_id }}: {{ $commLink->commLink->title }}
+            <ul style="display: flex">
+                <li style="display: inline"><a href="{{ config('api.rsi_url') }}{{ $commLink->commLink->url }}">@lang('RSI')</a></li>
+                <li style="display: inline">- <a href="{{ route('web.api.comm-links.show', $commLink->commLink->cig_id) }}">@lang('API')</a> -</li>
+                <li style="display: inline"><a href="{{ config('services.mediawiki.url') }}/Comm-Link:{{ $commLink->commLink->cig_id }}">@lang('Wiki')</a></li>
+            </ul>
+        </li>
+    @endforeach
+</ul>
+@endcomponent
 @endcomponent
