5b641b32c6abeeb2ea5cba2f16df2763bf9a2774
H. C. Kruse <hannes@octofox.de>
2023-11-25 21:42:36 +0100

    feat: Prepare importing additional missile data

---
+++ database/migrations/update/sc/2023_11_25_213235_add_params_to_sc_item_missiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sc_item_missiles', static function (Blueprint $table) {
+			$table->unsignedDouble('lock_range_max')->nullable()->after('lock_time');
+			$table->unsignedDouble('lock_range_min')->nullable()->after('lock_time');
+			$table->unsignedDouble('tracking_signal_min')->nullable()->after('lock_time');
+			$table->unsignedDouble('speed')->nullable()->after('lock_time');
+			$table->unsignedDouble('fuel_tank_size')->nullable()->after('lock_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sc_item_missiles', static function (Blueprint $table) {
+			$table->dropColumn('lock_range_max');
+			$table->dropColumn('lock_range_min');
+			$table->dropColumn('tracking_signal_min');
+			$table->dropColumn('speed');
+			$table->dropColumn('fuel_tank_size');
+        });
+    }
+};
