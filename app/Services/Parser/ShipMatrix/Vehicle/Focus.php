708e4e3193b60e25a45775a83ab818a576ccbd69
H. C. Kruse <hannes@octofox.de>
2023-12-02 09:41:48 +0100

    refactor: Also split vehicle focus by 'and'

--- app/Services/Parser/ShipMatrix/Vehicle/Focus.php
+++ app/Services/Parser/ShipMatrix/Vehicle/Focus.php
@@ -41,7 +41,7 @@ class Focus extends BaseElement
             return [];
         }
 
-        $vehicleFoci = array_map('trim', preg_split('/(\/|\s-\s|,)/', $rawFocus));
+        $vehicleFoci = array_map('trim', preg_split('/(\/|\s-\s|,|\sand\s)/', $rawFocus));
         $vehicleFociIDs = [];
 
         app('Log')::debug('Vehicle Focus count: ' . count($vehicleFoci));
