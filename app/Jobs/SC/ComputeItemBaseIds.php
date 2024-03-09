da7abe91a54bfaf3560605b423efcc9b33618402
H. C. Kruse <hannes@octofox.de>
2024-03-09 10:36:40 +0100

    fix: Fix matching class names for base id computation
    In reference to #101

--- app/Jobs/SC/ComputeItemBaseIds.php
+++ app/Jobs/SC/ComputeItemBaseIds.php
@@ -25,13 +25,23 @@ class ComputeItemBaseIds implements ShouldQueue
                 $class = explode('_', $item->class_name);
                 array_pop($class);
                 $class = implode('_', $class);
+                $classAlt = '';
+
+                $idEnd = strpos($item->class_name, '01');
+                if ($idEnd !== false) {
+                    $classAlt = substr($class, 0, $idEnd + 2);
+                }
 
                 $baseModel = Item::query()
                     ->where('uuid', '<>', $item->uuid)
                     ->where('type', $item->type)
-                    ->where(function (Builder $query) use ($class) {
+                    ->where(function (Builder $query) use ($class, $classAlt) {
                         $query->where('class_name', $class)
                             ->orWhere('class_name', sprintf('%s_01', $class));
+
+                        if (! empty($classAlt)) {
+                            $query->orWhere('class_name', $classAlt);
+                        }
                     })
                     ->first();
 
