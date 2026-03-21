6ee5530ae9e2a9abebbbdf666a7845f8157e6e96
H. C. Kruse <hannes@octofox.de>
2026-03-21 21:23:55 +0100

    chore: upgrade to laravel 13

--- app/Services/Parser/SC/Labels.php
+++ app/Services/Parser/SC/Labels.php
@@ -64,26 +64,26 @@ final class Labels
 
     private function loadFromDatabase(): void
     {
-        self::$staticLabelsLookup = Cache::remember('labels:all', now()->addHours(24), function () {
+        self::$staticLabelsLookup = collect(Cache::remember('labels:all', now()->addHours(24), function (): array {
             return GameLabel::all(['key', 'translation'])->mapWithKeys(function (GameLabel $label) {
                 return [$label->key => $label->getTranslation('translation', 'en')];
-            });
-        });
+            })->all();
+        }));
 
-        self::$staticZhTranslations = Cache::remember('labels:zh', now()->addHours(24), function () {
+        self::$staticZhTranslations = collect(Cache::remember('labels:zh', now()->addHours(24), function (): array {
             $labels = GameLabel::all(['key', 'translation']);
 
             return $labels->mapWithKeys(function (GameLabel $label) {
                 return [$label->key => $label->getTranslation('translation', 'zh')];
-            })->filter();
-        });
+            })->filter()->all();
+        }));
 
-        self::$staticDeTranslations = Cache::remember('labels:de', now()->addHours(24), function () {
+        self::$staticDeTranslations = collect(Cache::remember('labels:de', now()->addHours(24), function (): array {
             $labels = GameLabel::all(['key', 'translation']);
 
             return $labels->mapWithKeys(function (GameLabel $label) {
                 return [$label->key => $label->getTranslation('translation', 'de')];
-            })->filter();
-        });
+            })->filter()->all();
+        }));
     }
 }
