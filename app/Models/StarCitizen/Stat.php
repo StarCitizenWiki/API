0b1491d4f6ba5db2fa0cfb4672948110520e0827
H. C. Kruse <hannes@octofox.de>
2026-04-24 17:15:12 +0200

    chore: remove stale PHPDoc from Stat model

--- app/Models/StarCitizen/Stat.php
+++ app/Models/StarCitizen/Stat.php
@@ -7,9 +7,6 @@ namespace App\Models\StarCitizen;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Stats
- */
 class Stat extends Model
 {
     use HasFactory;
